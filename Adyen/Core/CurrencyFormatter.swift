663f0783ba8188ad54288bfb2adf61f4662856ee
Adyen Release Manager <git-manager@adyen.com>
2017-09-15 13:37:17 +0200

    Release 1.8.0

--- Adyen/Core/CurrencyFormatter.swift
+++ Adyen/Core/CurrencyFormatter.swift
@@ -9,10 +9,20 @@ import Foundation
 /// Convenience class to format a number with a given currency.
 internal class CurrencyFormatter {
     
+    // MARK: - Initialization
+    
+    private init() {
+        
+    }
+    
+    // MARK: - Internal
+    
     internal static func format(_ amount: Int, currencyCode: String) -> String? {
         currencyFormatter.currencyCode = currencyCode
         
-        let maximumFractionDigits = currencyFormatter.maximumFractionDigits
+        let maximumFractionDigits = CurrencyFormatter.maximumFractionDigits(for: currencyCode)
+        currencyFormatter.maximumFractionDigits = maximumFractionDigits
+        
         let decimalMinorAmount = NSDecimalNumber(value: amount)
         let convertedAmount = decimalMinorAmount.multiplying(byPowerOf10: Int16(-maximumFractionDigits)).doubleValue
         
@@ -20,6 +30,8 @@ internal class CurrencyFormatter {
         
         return currencyFormatter.string(from: number)
     }
+
+    // MARK: - Private
     
     private static let currencyFormatter: NumberFormatter = {
         let currencyFormatter = NumberFormatter()
@@ -28,8 +40,33 @@ internal class CurrencyFormatter {
         return currencyFormatter
     }()
     
-    private init() {
+    private static func maximumFractionDigits(for currencyCode: String) -> Int {
+        // For some currency codes iOS returns the wrong number of minor units.
+        // The below overrides are obtained from https://en.wikipedia.org/wiki/ISO_4217
         
+        switch currencyCode {
+        case "ISK", "CLP":
+            // iOS returns 0, which is in accordance with ISO-4217, but conflicts with our backend.
+            // TODO: remove this override once backend is fixed.
+            return 2
+        case "MRO":
+            // iOS returns 0 instead.
+            return 1
+        case "RSD":
+            // iOS returns 0 instead.
+            return 2
+        case "CVE":
+            // iOS returns 2 instead.
+            return 0
+        case "GHC":
+            // iOS returns 2 instead.
+            return 0
+        default:
+            let currencyFormatter = NumberFormatter()
+            currencyFormatter.numberStyle = .currency
+            currencyFormatter.currencyCode = currencyCode
+            return currencyFormatter.maximumFractionDigits
+        }
     }
     
 }
