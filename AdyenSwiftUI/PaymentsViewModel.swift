49262462f333116b3a19e5bc8f07cfb67cab9786
mohamede <mohamed.eldoheiri@adyen.com>
2020-10-08 13:06:46 +0200

    chore: removed unwanted imports from ComponentsViewController and PaymentsViewModel

--- AdyenSwiftUI/PaymentsViewModel.swift
+++ AdyenSwiftUI/PaymentsViewModel.swift
@@ -4,9 +4,6 @@
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
 
-import Adyen
-import AdyenCard
-import AdyenDropIn
 import SwiftUI
 
 internal final class PaymentsViewModel: ObservableObject, Identifiable, Presenter {
