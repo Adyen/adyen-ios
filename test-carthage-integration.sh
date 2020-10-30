0f3611c1d7182594d505e805c234b5dc3d297b4c
mohamede <mohamed.eldoheiri@adyen.com>
2020-10-30 09:11:22 +0100

    chore: created a script to test carthage integration and added a CI work flow to run this script with PR's and merging with main branches

---
+++ test-carthage-integration.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PROJECT_NAME=TempProject
+
+# Clean up.
+rm -rf $PROJECT_NAME
+
+mkdir -p $PROJECT_NAME && cd $PROJECT_NAME
+
+# Create the project.
+swift package init
+swift package generate-xcodeproj
+
+# Create the Package.swift.
+echo "git \"file:///../\"" > Cartfile
+
+xcodebuild archive -scheme TempProject-Package -destination 'generic/platform=iOS'
+
+xcodebuild archive -scheme TempProject-Package -destination 'generic/platform=iOS Simulator' ARCHS=i386
+
+xcodebuild archive -scheme TempProject-Package -destination 'generic/platform=iOS Simulator' ARCHS=x86_64
+
+# Clean up.
+cd ../
+rm -rf $PROJECT_NAME
