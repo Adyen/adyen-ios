ba3865ab1d13e20d14ee1c248ce8eed5cb876656
Mohamede <mohamed.eldoheiri@adyen.com>
2020-11-26 13:03:34 +0100

    chore: Made big refactoring in ThreeDS2Component, ThreeDS2ActionHandler and created a new ThreeDS2ClassicActionHandler

---
+++ AdyenCard/3DS2 Component/3DS2 Action handlers/ThreeDSActionHandlerResult.swift
@@ -0,0 +1,43 @@
+//
+// Copyright (c) 2020 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Adyen
+import Foundation
+
+internal enum ThreeDSActionHandlerResult: Decodable {
+
+    case action(Action)
+
+    case details(AdditionalDetails)
+
+    // MARK: - Coding
+
+    /// :nodoc:
+    internal init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(ActionType.self, forKey: .type)
+
+        switch type {
+        case .action:
+            let action = try container.decode(Action.self, forKey: .action)
+            self = .action(action)
+        case .completed:
+            let result = try container.decode(ThreeDSResult.self, forKey: .details)
+            self = .details(ThreeDS2Details.completed(result))
+        }
+    }
+
+    private enum ActionType: String, Decodable {
+        case action
+        case completed
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case details
+        case action
+    }
+}
