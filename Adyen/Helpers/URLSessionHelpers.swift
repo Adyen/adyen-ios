7ddd4c3017fdfd5f7eb2339854643840db641d63
Mohamede <mohamed.eldoheiri@adyen.com>
2020-12-02 14:40:11 +0100

    chore: Modified URLSessionHelpers to send back an error in case of a non 200 status response with non-valid JSON response body

--- Adyen/Helpers/URLSessionHelpers.swift
+++ Adyen/Helpers/URLSessionHelpers.swift
@@ -12,35 +12,35 @@ extension URLSession: AdyenCompatible {}
 public extension AdyenScope where Base: URLSession {
     func dataTask(with url: URL, completion: @escaping ((Result<Data, Error>) -> Void)) -> URLSessionDataTask {
         return base.dataTask(with: url, completionHandler: { data, response, error in
-            if let headers = (response as? HTTPURLResponse)?.allHeaderFields,
-               let path = response?.url?.path {
-                adyenPrint("---- Response Headers (/\(path)) ----")
-                adyenPrint(headers)
-            }
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data {
-                completion(.success(data))
-            } else {
-                fatalError("Invalid response.")
-            }
+            self.handle(data: data, response: response, error: error, completion: completion)
         })
     }
     
     func dataTask(with urlRequest: URLRequest, completion: @escaping ((Result<Data, Error>) -> Void)) -> URLSessionDataTask {
         return base.dataTask(with: urlRequest, completionHandler: { data, response, error in
-            if let headers = (response as? HTTPURLResponse)?.allHeaderFields,
-               let path = response?.url?.path {
-                adyenPrint("---- Response Headers (/\(path)) ----")
-                adyenPrint(headers)
-            }
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data {
-                completion(.success(data))
-            } else {
-                fatalError("Invalid response.")
-            }
+            self.handle(data: data, response: response, error: error, completion: completion)
         })
+    }
+
+    private func handle(data: Data?, response: URLResponse?, error: Error?, completion: @escaping ((Result<Data, Error>) -> Void)) {
+        let httpResponse = response as? HTTPURLResponse
+        if let headers = httpResponse?.allHeaderFields,
+           let path = response?.url?.path {
+            adyenPrint("---- Response Headers (/\(path)) ----")
+            adyenPrint(headers)
+        }
+        if let statusCode = httpResponse?.statusCode, statusCode != 200 {
+            let error = APIError(status: statusCode,
+                                 errorCode: "\(statusCode)",
+                                 errorMessage: "Http \(statusCode) error",
+                                 type: .urlError)
+            completion(.failure(error))
+        } else if let error = error {
+            completion(.failure(error))
+        } else if let data = data {
+            completion(.success(data))
+        } else {
+            fatalError("Invalid response.")
+        }
     }
 }
