e03bdc8dc38f2c97394544a9a67ac7bd3ae98444
Mohamede <mohamed.eldoheiri@adyen.com>
2020-12-21 13:34:01 +0100

    chore: refactored ThreeDS2CompactActionHandler and ThreeDS2ClassicActionHandler and put the common code in ThreeDS2CoreActionHandler

---
+++ AdyenCard/3DS2 Component/3DS2 Action handlers/AnyThreeDS2ActionHandler.swift
@@ -0,0 +1,61 @@
+//
+// Copyright (c) 2020 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Adyen
+import Foundation
+
+/// :nodoc:
+internal protocol AnyThreeDS2ActionHandler {
+
+    /// :nodoc:
+    func handle(_ fingerprintAction: ThreeDS2FingerprintAction,
+                completionHandler: @escaping (Result<ThreeDSActionHandlerResult, Error>) -> Void)
+
+    /// :nodoc:
+    func handle(_ challengeAction: ThreeDS2ChallengeAction,
+                completionHandler: @escaping (Result<ThreeDSActionHandlerResult, Error>) -> Void)
+}
+
+/// :nodoc:
+internal protocol ComponentWrapper: Component {
+
+    var wrappedComponent: Component { get }
+}
+
+/// :nodoc:
+extension ComponentWrapper {
+    internal var environment: Environment {
+        get {
+            wrappedComponent.environment
+        }
+
+        set {
+            wrappedComponent.environment = newValue
+        }
+    }
+
+    /// :nodoc:
+    internal var clientKey: String? {
+        get {
+            wrappedComponent.clientKey
+        }
+
+        set {
+            wrappedComponent.clientKey = newValue
+        }
+    }
+
+    /// :nodoc:
+    internal var _isDropIn: Bool { // swiftlint:disable:this identifier_name
+        get {
+            wrappedComponent._isDropIn
+        }
+
+        set {
+            wrappedComponent._isDropIn = newValue
+        }
+    }
+}
