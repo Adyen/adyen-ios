b5657bc6497f9eedae9601e025cfbd1de009bcd3
mohamede <mohamed.eldoheiri@adyen.com>
2021-02-17 09:11:35 +0100

    chore: deleted PaymentsController.swift and reverted the Configuration.swift

--- Demo/Configuration.swift
+++ Demo/Configuration.swift
@@ -12,34 +12,32 @@ internal enum Configuration {
     // swiftlint:disable explicit_acl
 
     /// Please use your own web server between your app and adyen checkout API.
-    static let demoServerEnvironment = DemoServerEnvironment.local
+    static let demoServerEnvironment = DemoServerEnvironment.test
 
-    static let local = Environment(baseURL: URL(string: "http://localhost:8080/")!)
-
-    static let componentsEnvironment = local
+    static let componentsEnvironment = Environment.test
 
     static let appName = "Adyen Demo"
 
-    static let amount = Payment.Amount(value: 17408, currencyCode: "IDR")
+    static let amount = Payment.Amount(value: 17408, currencyCode: "EUR")
 
     static let reference = "Test Order Reference - iOS UIHost"
     
-    static let countryCode = "ID"
+    static let countryCode = "NL"
 
     static let returnUrl = "ui-host://"
     
     static let shopperReference = "iOS Checkout Shopper"
 
-    static let merchantAccount = "TestMerchant"
+    static let merchantAccount = "TestMerchantCheckout"
 
     static let shopperEmail = "checkoutshopperios@example.org"
     
     static let additionalData = ["allow3DS2": true]
-    
-    static let clientKey = "test_L6HTEOAXQBCZJHKNU4NLN6EI7IE6VRRW"
+
+    static let clientKey = "{YOUR_CLIENT_KEY}"
 
     // swiftlint:disable:next line_length
-    static let demoServerAPIKey = "AQEthmfxKo7MbhFLw0m/n3Q5qf3VfI5eGbBFVXVXyGHNhisxSHQZLQhnJZKhUXeVEMFdWw2+5HzctViMSCJMYAc=-iLjeIbd8YjLi4aPYhovzGTmJkrNSaNExFw267yN7eYY=-t4aYUIMHdBz6MudS"
+    static let demoServerAPIKey = "{YOUR_DEMO_SERVER_API_KEY}"
     
     static let applePayMerchantIdentifier = "{YOUR_APPLE_PAY_MERCHANT_IDENTIFIER}"
     
