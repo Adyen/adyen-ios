2582d3ed74ae982fffa6b1e2dbf2ce5bb06a025a
vladimir <vladimir.abramichev@adyen.com>
2021-03-03 15:05:40 +0100

    chore: improve tests

---
+++ Tests/AdyenTests/Adyen Tests/Components/Base/IssuerListComponentTests.swift
@@ -0,0 +1,37 @@
+//
+// Copyright © 2020 Adyen. All rights reserved.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+@testable import Adyen
+@testable import AdyenComponents
+import XCTest
+
+class IssuerListComponentTests: XCTestCase {
+
+    func testStartStopLoading() {
+        let paymentMethod = try! Coder.decode(issuerListDictionary) as IssuerListPaymentMethod
+        let sut = IssuerListComponent(paymentMethod: paymentMethod)
+
+        let expectation = XCTestExpectation(description: "Dummy Expectation")
+        UIApplication.shared.keyWindow?.rootViewController = sut.viewController
+
+        let listViewController = sut.viewController as? ListViewController
+        XCTAssertNotNil(listViewController)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(1)) {
+            let item = listViewController!.sections[0].items[0]
+            let cell = listViewController!.tableView.visibleCells[0] as! ListCell
+            XCTAssertFalse(cell.showsActivityIndicator)
+            listViewController?.startLoading(for: item)
+            XCTAssertTrue(cell.showsActivityIndicator)
+            sut.stopLoading {
+                XCTAssertFalse(cell.showsActivityIndicator)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+}
