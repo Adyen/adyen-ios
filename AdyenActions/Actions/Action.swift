0d773fb57bbaf259f16d343289708a67595853c7
Mohamed Eldoheiri <mohamed.eldoheiri@adyen.com>
2021-06-10 13:41:44 +0200

    chore: fixed formatting of Action enum

--- AdyenActions/Actions/Action.swift
+++ AdyenActions/Actions/Action.swift
@@ -79,7 +79,7 @@ public enum Action: Decodable {
         case sdk
         case qrCode
         case await
-            case voucher
+        case voucher
     }
 
     private enum CodingKeys: String, CodingKey {
