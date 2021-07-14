c71d2e31f13109e9151958f915fc7d579a341a38
naufal <naufal.aros@adyen.com>
2021-07-14 14:41:48 +0200

    chore: Set form items

--- AdyenComponents/Affirm/AffirmComponent.swift
+++ AdyenComponents/Affirm/AffirmComponent.swift
@@ -13,6 +13,8 @@ import UIKit
 /// A component that provides a form for Affirm payment.
 public final class AffirmComponent: AbstractPersonalInformationComponent {
     
+    // MARK: - Properties
+    
     // MARK: - Initializers
     
     public init(paymentMethod: PaymentMethod,
@@ -20,21 +22,71 @@ public final class AffirmComponent: AbstractPersonalInformationComponent {
                 style: FormComponentStyle) {
         // TODO: - Init logic
         // 1. Set configuration
-        let configuration = Configuration(fields: AffirmComponent.fields)
+        let personalDetailsHeaderItem = FormLabelItem(text: "Personal details", style: style.sectionHeader).addingDefaultMargins()
         
+        let deliveryAddressItem = FormAddressItem(initialCountry: "US", style: style.addressStyle)
+        deliveryAddressItem.title = "Delivery address"
+        let fields: [PersonalInformation] = [.custom(CustomFormItemInjector(item: personalDetailsHeaderItem)),
+                                             .firstName,
+                                             .lastName,
+                                             .email,
+                                             .phone,
+                                             .address,
+                                             .custom(CustomFormItemInjector(item: deliveryAddressItem))]
+        
+        let configuration = Configuration(fields: fields)
         super.init(paymentMethod: paymentMethod,
                    configuration: configuration,
-                   apiContext: apiContext)
+                   apiContext: apiContext,
+                   style: style)
     }
     
+    // MARK: - Public
+    
     public override func submitButtonTitle() -> String {
-        "SUBMIT DATA"
+        return "Confirm purchase"
+    }
+    
+    public override func createPaymentDetails() -> PaymentMethodDetails {
+        guard let firstNameItem = firstNameItem else {
+            fatalError()
+        }
+        guard let lastNameItem = lastNameItem else {
+            fatalError()
+        }
+        guard let emailItem = emailItem else {
+            fatalError()
+        }
+        guard let phoneItem = phoneItem else {
+            fatalError()
+        }
+        guard let addressItem = addressItem else {
+            fatalError()
+        }
+        
+        let shopperName = ShopperName(firstName: firstNameItem.value,
+                                      lastName: lastNameItem.value)
+        let emailAddress = emailItem.value
+        let telephoneNumber = phoneItem.value
+        let billingAddress = addressItem.value
+        
+        let affirmDetails = AffirmDetails(paymentMethod: paymentMethod,
+                                          shopperName: shopperName,
+                                          telephoneNumber: telephoneNumber,
+                                          emailAddress: emailAddress,
+                                          billingAddress: billingAddress,
+                                          deliveryAddress: nil)
+        return affirmDetails
+    }
+    
+    public override func getPhoneExtensions() -> [PhoneExtension] {
+        let query = PhoneExtensionsQuery(paymentMethod: .generic)
+        return PhoneExtensionsRepository.get(with: query)
     }
     
     // MARK: - Private
     
     private static var fields: [PersonalInformation] {
-        let fields: [PersonalInformation] = [.firstName, .lastName, .email]
-        return fields
+        return []
     }
 }
