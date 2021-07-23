35f3d7fe40cfb4a88651bf5eb43f7fee5c6f76e2
erenbesel <erenbesel@gmail.com>
2021-07-23 11:51:09 +0200

    feature: disable luhn check if flag is false in binlookup

--- Tests/AdyenTests/Card Tests/Validators/CardNumberValidatorTests.swift
+++ Tests/AdyenTests/Card Tests/Validators/CardNumberValidatorTests.swift
@@ -10,7 +10,7 @@ import XCTest
 class CardNumberValidatorTests: XCTestCase {
     
     func testValidCards() {
-        let validator = CardNumberValidator()
+        let validator = CardNumberValidator(isLuhnCheckEnabled: true)
         
         CardNumbers.valid.forEach { cardNumber in
             XCTAssertTrue(validator.isValid(cardNumber))
@@ -18,10 +18,18 @@ class CardNumberValidatorTests: XCTestCase {
     }
     
     func testInvalidCards() {
-        let validator = CardNumberValidator()
+        let validator = CardNumberValidator(isLuhnCheckEnabled: true)
         
         CardNumbers.invalid.forEach { cardNumber in
             XCTAssertFalse(validator.isValid(cardNumber))
         }
+    }
+    
+    func testNonLuhnCheckCards() {
+        let validator = CardNumberValidator(isLuhnCheckEnabled: false)
+        XCTAssertTrue(validator.isValid("4111111111111112"))
+        XCTAssertTrue(validator.isValid("370000000000000"))
+        XCTAssertFalse(validator.isValid("37000000000"))
+        XCTAssertFalse(validator.isValid("0"))
     }
 }
