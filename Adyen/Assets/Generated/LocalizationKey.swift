f71bb3f356e9423502ad54a5cc1d408f0235a24d
naufal <naufal.aros@adyen.com>
2021-08-06 13:34:20 +0200

    chore: Update toggle title content key

--- Adyen/Assets/Generated/LocalizationKey.swift
+++ Adyen/Assets/Generated/LocalizationKey.swift
@@ -117,7 +117,7 @@ public struct LocalizationKey {
     public static let partialPaymentRemainingBalance = LocalizationKey(key: "adyen.partialPayment.remainingBalance")
     public static let partialPaymentPayRemainingAmount = LocalizationKey(key: "adyen.partialPayment.payRemainingAmount")
     public static let amount = LocalizationKey(key: "adyen.amount")
-    public static let deliveryAddressToggleTitle = LocalizationKey(key: "adyen.deliveryAddressToggle.title")
+    public static let deliveryAddressToggleTitle = LocalizationKey(key: "adyen.affirm.deliveryAddressToggle.title")
     public static let cardTaxNumberLabel = LocalizationKey(key: "adyen.card.taxNumber.label")
     public static let cardTaxNumberPlaceholder = LocalizationKey(key: "adyen.card.taxNumber.placeholder")
     public static let cardTaxNumberInvalid = LocalizationKey(key: "adyen.card.taxNumber.invalid")
