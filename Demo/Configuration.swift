ddb87c43a6f336834e24bc4bf6bdadc083a452f4
mohamede <mohamed.eldoheiri@adyen.com>
2021-08-12 14:31:33 +0200

    chore: revert environment to test in ConfigurationConstants

--- Demo/Configuration.swift
+++ Demo/Configuration.swift
@@ -13,9 +13,9 @@ internal enum ConfigurationConstants {
     // swiftlint:disable line_length
 
     /// Please use your own web server between your app and adyen checkout API.
-    static let demoServerEnvironment = DemoServerEnvironment.local
+    static let demoServerEnvironment = DemoServerEnvironment.test
 
-    static let componentsEnvironment = Environment(baseURL: URL(string: "http://localhost:8080")!)
+    static let componentsEnvironment = Environment.test
 
     static let appName = "Adyen Demo"
 
