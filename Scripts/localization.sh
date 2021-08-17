86796d129a9e5a23221b19a88dc99ff37e244878
Naufal Aros <naufal.aros@adyen.com>
2021-08-17 13:54:38 +0200

    echo: Add progress to cloning

--- Scripts/localization.sh
+++ Scripts/localization.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-# CONSTANTS
-translations-ssh="git@code.is.adyen.com:Checkout/translations.git"
+echo "##################################################"
+echo "################ LOCALIZATION JOB ##################"
+echo "##################################################\n\n"
 
 echo "---------------- CLONING TRANSLATIONS REPO ----------------\n"
 
-git clone git@code.is.adyen.com:Checkout/translations.git $SRCROOT/translations
-
+git clone --progress --verbose git@code.is.adyen.com:Checkout/translations.git $SRCROOT/translations
 
 echo "\n\n---------------- GENERATING LOCALIZATION KEYS ----------------\n"
 
 cd translations/iOS
 swift StringsGenerator.swift $PROJECT_DIR 
-
 
 echo "\n\n---------------- REMOVING TRANSLATIONS REPO ----------------\n"
 
