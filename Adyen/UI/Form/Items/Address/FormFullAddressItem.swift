3333d7ddb8ce85ff4f1891a48b09eccf1c16b0d1
erenbesel <erenbesel@gmail.com>
2021-08-26 14:49:31 +0200

    chore: fix failing tests

--- Adyen/UI/Form/Items/Address/FormFullAddressItem.swift
+++ Adyen/UI/Form/Items/Address/FormFullAddressItem.swift
@@ -53,10 +53,9 @@ public final class FormAddressItem: FormValueItem<PostalAddress, AddressStyle>, 
         self.identifier = identifier
         update(for: initialCountry)
         
+        bind(countrySelectItem.publisher, at: \.identifier, to: self, at: \.value.country)
         observe(countrySelectItem.publisher, eventHandler: { [weak self] event in
-            guard let self = self else { return }
-            self.value.country = event.element.identifier
-            self.update(for: event.element.identifier)
+            self?.update(for: event.element.identifier)
         })
     }
     
