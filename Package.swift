90a10d440ef53fae13514a2a9049b864f505451f
mohamede <mohamed.eldoheiri@adyen.com>
2021-11-02 10:52:53 +0100

    Bumps 3DS2 SDK version to 2.2.4

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
         .package(
             name: "Adyen3DS2",
             url: "https://github.com/Adyen/adyen-3ds2-ios",
-            .exact(Version(2, 2, 3))
+            .exact(Version(2, 2, 4))
         ),
         .package(
             name: "AdyenNetworking",
