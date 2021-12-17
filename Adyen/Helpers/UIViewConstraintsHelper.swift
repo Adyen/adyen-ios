b2a866f54d92cf3a64879284f9897655827e6bbe
erenbesel <erenbesel@gmail.com>
2021-12-17 17:45:48 +0100

    chore: update anchor function

--- Adyen/Helpers/UIViewConstraintsHelper.swift
+++ Adyen/Helpers/UIViewConstraintsHelper.swift
@@ -15,73 +15,47 @@ extension AdyenScope where Base: UIView {
     /// - IMPORTANT: Both views should be in the same hierarchy.
     /// - Parameter view: Container view
     /// - Parameter padding: Padding values for each edge. Default is 0 on all edges.
-    /// - Parameter edges: Edges on which the views should be anchored. Defaults to all 4 edge anchors.
     @discardableResult
     public func anchor(inside view: UIView, with padding: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
-        let edges: Set<AnchorEdge> = [.top(padding.top),
-                                      .left(padding.left),
-                                      .bottom(padding.bottom),
-                                      .right(padding.right)]
-        return anchor(inside: .view(view), onEdges: edges)
+        anchor(inside: .view(view), edgeInsets: EdgeInsets(edgeInsets: padding))
     }
 
     /// Attaches top, bottom, left and right anchors of this view to the corresponding anchors inside the specified layout guide.
     /// - IMPORTANT: Both views should be in the same hierarchy.
     /// - Parameter margins: The layout guide to constraint to.
     /// - Parameter padding: Padding values for each edge. Default is 0 on all edges.
-    /// - Parameter edges: Edges on which the views should be anchored. Defaults to all 4 edge anchors.
     @discardableResult
     public func anchor(inside margins: UILayoutGuide, with padding: UIEdgeInsets = .zero) -> [NSLayoutConstraint] {
-        let edges: Set<AnchorEdge> = [.top(padding.top),
-                                      .left(padding.left),
-                                      .bottom(padding.bottom),
-                                      .right(padding.right)]
-        return anchor(inside: .layoutGuide(margins), onEdges: edges)
+        anchor(inside: .layoutGuide(margins), edgeInsets: EdgeInsets(edgeInsets: padding))
     }
     
     /// Attaches the given edges of this view to coresponding anchors inside the specified anchor source.
     /// - Parameters:
     ///   - anchorSource: The anchor source to contain this view.
-    ///   - edges: Edges with inset values on which the views should be anchored. Defaults to all 4 edges with 0 inset each.
+    ///   - edgeInsets: Edges with inset values on which the views should be anchored. Defaults to all 4 edges with 0 inset each.
     @discardableResult
     public func anchor(inside anchorSource: LayoutAnchorSource,
-                       onEdges edges: Set<AnchorEdge> = [.top(0),
-                                                         .left(0),
-                                                         .bottom(0),
-                                                         .right(0)]) -> [NSLayoutConstraint] {
+                       edgeInsets: EdgeInsets = .zero) -> [NSLayoutConstraint] {
         base.translatesAutoresizingMaskIntoConstraints = false
         
-        let topAnchor: NSLayoutYAxisAnchor
-        let leadingAnchor: NSLayoutXAxisAnchor
-        let bottomAnchor: NSLayoutYAxisAnchor
-        let trailingAnchor: NSLayoutXAxisAnchor
-        
-        switch anchorSource {
-        case let .view(view):
-            topAnchor = view.topAnchor
-            leadingAnchor = view.leadingAnchor
-            bottomAnchor = view.bottomAnchor
-            trailingAnchor = view.trailingAnchor
-        case let .layoutGuide(uILayoutGuide):
-            topAnchor = uILayoutGuide.topAnchor
-            leadingAnchor = uILayoutGuide.leadingAnchor
-            bottomAnchor = uILayoutGuide.bottomAnchor
-            trailingAnchor = uILayoutGuide.trailingAnchor
-        }
-        
         var constraints: [NSLayoutConstraint] = []
-        for edge in edges {
-            switch edge {
-            case let .top(padding):
-                constraints.append(base.topAnchor.constraint(equalTo: topAnchor, constant: padding))
-            case let .left(padding):
-                constraints.append(base.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding))
-            case let .bottom(padding):
-                constraints.append(base.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding))
-            case let .right(padding):
-                constraints.append(base.trailingAnchor.constraint(equalTo: trailingAnchor, constant: padding))
-            }
+        if let top = edgeInsets.top {
+            constraints.append(base.topAnchor.constraint(equalTo: anchorSource.anchorSet.topAnchor,
+                                                         constant: top))
         }
+        if let left = edgeInsets.left {
+            constraints.append(base.leadingAnchor.constraint(equalTo: anchorSource.anchorSet.leadingAnchor,
+                                                             constant: left))
+        }
+        if let bottom = edgeInsets.bottom {
+            constraints.append(base.bottomAnchor.constraint(equalTo: anchorSource.anchorSet.bottomAnchor,
+                                                            constant: bottom))
+        }
+        if let right = edgeInsets.right {
+            constraints.append(base.trailingAnchor.constraint(equalTo: anchorSource.anchorSet.trailingAnchor,
+                                                              constant: right))
+        }
+        
         NSLayoutConstraint.activate(constraints)
         return constraints
     }
@@ -97,32 +71,72 @@ extension AdyenScope where Base: UIView {
         anchor(inside: containerView, with: insets)
         return containerView
     }
+
+    /// An enum to specify an anchor source
+    /// :nodoc:
+    public enum LayoutAnchorSource {
+        
+        /// Regular `UIView` object
+        case view(UIView)
+        
+        /// Specified layout guide such as Layout Margins or Safe Area.
+        case layoutGuide(UILayoutGuide)
+        
+        fileprivate var anchorSet: AnyAnchorSet {
+            switch self {
+            case let .view(view as AnyAnchorSet):
+                return view
+            case let .layoutGuide(layoutGuide):
+                return layoutGuide
+            }
+        }
+    }
+    
+    /// Inset distances for views that can be nil.
+    /// :nodoc:
+    public struct EdgeInsets {
+        
+        /// :nodoc:
+        public var top: CGFloat?
+
+        /// :nodoc:
+        public var left: CGFloat?
+
+        /// :nodoc:
+        public var bottom: CGFloat?
+
+        /// :nodoc:
+        public var right: CGFloat?
+        
+        /// Creates insets with 0 on all 4 values.
+        /// :nodoc:
+        public static var zero: EdgeInsets {
+            .init(top: 0, left: 0, bottom: 0, right: 0)
+        }
+        
+        internal init(edgeInsets: UIEdgeInsets) {
+            top = edgeInsets.top
+            left = edgeInsets.left
+            bottom = edgeInsets.bottom
+            right = edgeInsets.right
+        }
+        
+        /// :nodoc:
+        public init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil) {
+            self.top = top
+            self.left = left
+            self.bottom = bottom
+            self.right = right
+        }
+    }
 }
 
-/// Edges combined with their inset values.
-/// :nodoc:
-public enum AnchorEdge: Hashable {
-    
-    /// Top edge with its inset value.
-    case top(CGFloat)
-    
-    /// Left edge with its inset value.
-    case left(CGFloat)
-    
-    /// Bottom edge with its inset value.
-    case bottom(CGFloat)
-    
-    /// Right edge with its inset value.
-    case right(CGFloat)
+private protocol AnyAnchorSet {
+    var topAnchor: NSLayoutYAxisAnchor { get }
+    var leadingAnchor: NSLayoutXAxisAnchor { get }
+    var bottomAnchor: NSLayoutYAxisAnchor { get }
+    var trailingAnchor: NSLayoutXAxisAnchor { get }
 }
 
-/// An enum to specify an anchor source
-/// :nodoc:
-public enum LayoutAnchorSource {
-    
-    /// Regular `UIView` object
-    case view(UIView)
-    
-    /// Specified layout guide such as Layout Margins or Safe Area.
-    case layoutGuide(UILayoutGuide)
-}
+extension UIView: AnyAnchorSet {}
+extension UILayoutGuide: AnyAnchorSet {}
