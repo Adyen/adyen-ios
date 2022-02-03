81ca3efd8ada18736c9b16e46b0ae7f4b5de3fb2
mohamede <mohamed.eldoheiri@adyen.com>
2022-02-03 16:20:29 +0100

    chore: small fix in WeChatPayActionComponent.swift to exclude all simulators at compile time

--- AdyenWeChatPay/WeChatPayActionComponent/WeChatPayActionComponent.swift
+++ AdyenWeChatPay/WeChatPayActionComponent/WeChatPayActionComponent.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2021 Adyen N.V.
+// Copyright (c) 2022 Adyen N.V.
 //
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
@@ -11,102 +11,102 @@
 import Adyen
 import Foundation
 
-#if !(targetEnvironment(simulator) && arch(arm64)) && canImport(AdyenWeChatPayInternal)
+#if !targetEnvironment(simulator) && canImport(AdyenWeChatPayInternal)
 
-import AdyenWeChatPayInternal
+    import AdyenWeChatPayInternal
 
-/// Action component to handle WeChat Pay SDK action.
-public final class WeChatPaySDKActionComponent: NSObject, AnyWeChatPaySDKActionComponent {
+    /// Action component to handle WeChat Pay SDK action.
+    public final class WeChatPaySDKActionComponent: NSObject, AnyWeChatPaySDKActionComponent {
 
-    private static let universalLink = "https://www.adyen.com/"
+        private static let universalLink = "https://www.adyen.com/"
     
-    /// :nodoc:
-    public let apiContext: APIContext
+        /// :nodoc:
+        public let apiContext: APIContext
     
-    /// :nodoc:
-    public weak var delegate: ActionComponentDelegate?
+        /// :nodoc:
+        public weak var delegate: ActionComponentDelegate?
     
-    /// :nodoc:
-    private var currentlyHandledAction: WeChatPaySDKAction?
+        /// :nodoc:
+        private var currentlyHandledAction: WeChatPaySDKAction?
     
-    /// :nodoc:
-    public init(apiContext: APIContext) {
-        self.apiContext = apiContext
-    }
+        /// :nodoc:
+        public init(apiContext: APIContext) {
+            self.apiContext = apiContext
+        }
     
-    /// :nodoc:
-    public func handle(_ action: WeChatPaySDKAction) {
-        guard Self.isDeviceSupported() else {
-            delegate?.didFail(with: ComponentError.paymentMethodNotSupported, from: self)
-            return
+        /// :nodoc:
+        public func handle(_ action: WeChatPaySDKAction) {
+            guard Self.isDeviceSupported() else {
+                delegate?.didFail(with: ComponentError.paymentMethodNotSupported, from: self)
+                return
+            }
+
+            assert(Thread.isMainThread, "present(actionData:) must be called on the main thread.")
+            assert(currentlyHandledAction == nil, """
+              Warning: There was a WeChatPaySDKAction already in progress.
+             Handling multiple WeChatPaySDKAction's in parallel is not supported.
+            """)
+        
+            Analytics.sendEvent(component: "wechatpaySDK", flavor: _isDropIn ? .dropin : .components, context: apiContext)
+        
+            currentlyHandledAction = action
+        
+            RedirectListener.registerForURL { url in
+                WXApi.handleOpen(url, delegate: self)
+            }
+        
+            WXApi.registerApp(action.sdkData.appIdentifier, universalLink: WeChatPaySDKActionComponent.universalLink)
+            WXApi.send(PayReq(actionData: action.sdkData))
+        
+            delegate?.didOpenExternalApplication(self)
+        }
+    
+        /// Checks if the current device supports WeChat Pay.
+        public static func isDeviceSupported() -> Bool {
+            assertWeChatPayAppSchemeConfigured()
+            WXApi.registerApp("", universalLink: WeChatPaySDKActionComponent.universalLink)
+            return WXApi.isWXAppInstalled() && WXApi.isWXAppSupport()
+        }
+    
+        private static func assertWeChatPayAppSchemeConfigured() {
+            guard Bundle.main.adyen.isSchemeConfigured("weixin") else {
+                // swiftlint:disable:next line_length
+                return AdyenAssertion.assertionFailure(message: "weixin:// scheme must be added to Info.plist under LSApplicationQueriesSchemes key.")
+            }
         }
 
-        assert(Thread.isMainThread, "present(actionData:) must be called on the main thread.")
-        assert(currentlyHandledAction == nil, """
-          Warning: There was a WeChatPaySDKAction already in progress.
-         Handling multiple WeChatPaySDKAction's in parallel is not supported.
-        """)
-        
-        Analytics.sendEvent(component: "wechatpaySDK", flavor: _isDropIn ? .dropin : .components, context: apiContext)
-        
-        currentlyHandledAction = action
-        
-        RedirectListener.registerForURL { url in
-            WXApi.handleOpen(url, delegate: self)
-        }
-        
-        WXApi.registerApp(action.sdkData.appIdentifier, universalLink: WeChatPaySDKActionComponent.universalLink)
-        WXApi.send(PayReq(actionData: action.sdkData))
-        
-        delegate?.didOpenExternalApplication(self)
     }
-    
-    /// Checks if the current device supports WeChat Pay.
-    public static func isDeviceSupported() -> Bool {
-        assertWeChatPayAppSchemeConfigured()
-        WXApi.registerApp("", universalLink: WeChatPaySDKActionComponent.universalLink)
-        return WXApi.isWXAppInstalled() && WXApi.isWXAppSupport()
-    }
-    
-    private static func assertWeChatPayAppSchemeConfigured() {
-        guard Bundle.main.adyen.isSchemeConfigured("weixin") else {
-            // swiftlint:disable:next line_length
-            return AdyenAssertion.assertionFailure(message: "weixin:// scheme must be added to Info.plist under LSApplicationQueriesSchemes key.")
-        }
-    }
-
-}
-
-/// :nodoc:
-extension WeChatPaySDKActionComponent: WXApiDelegate {
 
     /// :nodoc:
-    public func onResp(_ resp: BaseResp) {
-        guard let currentlyHandledAction = currentlyHandledAction else {
-            return AdyenAssertion.assertionFailure(message: "no WeChatPaySDKAction were handled")
-        }
-        let additionalData = WeChatPayAdditionalDetails(resultCode: String(resp.errCode))
-        let actionData = ActionComponentData(details: additionalData, paymentData: currentlyHandledAction.paymentData)
-        delegate?.didProvide(actionData, from: self)
-        self.currentlyHandledAction = nil
-    }
-    
-}
+    extension WeChatPaySDKActionComponent: WXApiDelegate {
 
-/// :nodoc:
-private extension PayReq {
-    /// :nodoc:
-    convenience init(actionData: WeChatPaySDKData) {
-        self.init()
-        
-        openID = actionData.appIdentifier
-        partnerId = actionData.partnerIdentifier
-        prepayId = actionData.prepayIdentifier
-        timeStamp = UInt32(actionData.timestamp)!
-        package = actionData.package
-        nonceStr = actionData.nonce
-        sign = actionData.signature
+        /// :nodoc:
+        public func onResp(_ resp: BaseResp) {
+            guard let currentlyHandledAction = currentlyHandledAction else {
+                return AdyenAssertion.assertionFailure(message: "no WeChatPaySDKAction were handled")
+            }
+            let additionalData = WeChatPayAdditionalDetails(resultCode: String(resp.errCode))
+            let actionData = ActionComponentData(details: additionalData, paymentData: currentlyHandledAction.paymentData)
+            delegate?.didProvide(actionData, from: self)
+            self.currentlyHandledAction = nil
+        }
+    
     }
-}
+
+    /// :nodoc:
+    private extension PayReq {
+        /// :nodoc:
+        convenience init(actionData: WeChatPaySDKData) {
+            self.init()
+        
+            openID = actionData.appIdentifier
+            partnerId = actionData.partnerIdentifier
+            prepayId = actionData.prepayIdentifier
+            timeStamp = UInt32(actionData.timestamp)!
+            package = actionData.package
+            nonceStr = actionData.nonce
+            sign = actionData.signature
+        }
+    }
 
 #endif
