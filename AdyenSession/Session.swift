681c77e1794293171a443774de77a395448d3878
mohamede <mohamed.eldoheiri@adyen.com>
2022-03-01 12:57:31 +0100

    feature: Creates the Session Module

---
+++ AdyenSession/Session.swift
@@ -0,0 +1,13 @@
+//
+//  Session.swift
+//  AdyenSession
+//
+//  Created by Mohamed Eldoheiri on 3/1/22.
+//  Copyright © 2022 Adyen. All rights reserved.
+//
+
+import Foundation
+import Adyen
+#if canImport(AdyenActions)
+    import AdyenActions
+#endif
