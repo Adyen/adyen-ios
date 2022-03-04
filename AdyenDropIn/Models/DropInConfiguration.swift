5ff43cf83eef9bdf197b4609528f1c8dc2fa38d8
erenbesel <erenbesel@gmail.com>
2022-03-04 17:28:26 +0100

    fix naming conflict

--- AdyenDropIn/Models/DropInConfiguration.swift
+++ AdyenDropIn/Models/DropInConfiguration.swift
@@ -32,7 +32,7 @@ public extension DropInComponent {
         public var paymentMethodsList = PaymentMethodListConfiguration()
         
         /// Action components related configurations.
-        public var actionComponent = ActionComponent()
+        public var actionComponent = ActionComponentConfiguration()
         
         /// Shopper related information
         public var shopper: PrefilledShopperInformation?
@@ -75,7 +75,7 @@ public extension DropInComponent {
 
     }
     
-    struct ActionComponent {
+    struct ActionComponentConfiguration {
         /// Three DS configurations
         public var threeDS: AdyenActionComponent.Configuration.ThreeDS = .init()
     }
