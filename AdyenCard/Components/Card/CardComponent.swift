3904e5499eb75993f7baa38810f95f3342028eca
vladimir <vladimir.abramichev@adyen.com>
2022-04-20 12:04:02 +0200

    fix: populate underling StoredComponent with localizationParameters

--- AdyenCard/Components/Card/CardComponent.swift
+++ AdyenCard/Components/Card/CardComponent.swift
@@ -135,9 +135,13 @@ public class CardComponent: PublicKeyConsumer,
         }
         var component: PaymentComponent & PresentableComponent
         if configuration.stored.showsSecurityCodeField {
-            component = StoredCardComponent(storedCardPaymentMethod: paymentMethod, apiContext: apiContext)
+            let storedComponent = StoredCardComponent(storedCardPaymentMethod: paymentMethod, apiContext: apiContext)
+            storedComponent.localizationParameters = configuration.localizationParameters
+            component = storedComponent
         } else {
-            component = StoredPaymentMethodComponent(paymentMethod: paymentMethod, apiContext: apiContext)
+            let storedComponent = StoredPaymentMethodComponent(paymentMethod: paymentMethod, apiContext: apiContext)
+            storedComponent.localizationParameters = configuration.localizationParameters
+            component = storedComponent
         }
         component.payment = payment
         return component
