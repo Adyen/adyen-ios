f1f92b74be97f541d1b50e2aaf9ee4e1456e876f
Naufal Aros <naufal.aros@adyen.com>
2022-04-21 11:42:30 +0200

    chore: Import UIKit in TelemetryData

--- Adyen/Analytics/AnalyticsProvider/TelemetryData.swift
+++ Adyen/Analytics/AnalyticsProvider/TelemetryData.swift
@@ -4,6 +4,7 @@
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
 
+import UIKit
 import Foundation
 
 internal struct TelemetryData: Encodable {
