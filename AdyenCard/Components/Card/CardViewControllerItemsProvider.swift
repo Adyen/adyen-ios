2e0c8c51ec84aab8ac5dc1fe17569ab05a4e69ed
neelam <neelam.sharma@adyen.com>
2022-05-19 10:25:57 +0200

    Fixed cardholder name validation

--- AdyenCard/Components/Card/CardViewControllerItemsProvider.swift
+++ AdyenCard/Components/Card/CardViewControllerItemsProvider.swift
@@ -117,7 +117,7 @@ extension CardViewController {
             let holderNameItem = FormTextInputItem(style: formStyle.textField)
             holderNameItem.title = localizedString(.cardNameItemTitle, localizationParameters)
             holderNameItem.placeholder = localizedString(.cardNameItemPlaceholder, localizationParameters)
-            holderNameItem.validator = LengthValidator(minimumLength: 2)
+            holderNameItem.validator = LengthValidator(minimumLength: 1)
             holderNameItem.validationFailureMessage = localizedString(.cardNameItemInvalid, localizationParameters)
             holderNameItem.autocapitalizationType = .words
             holderNameItem.identifier = ViewIdentifierBuilder.build(scopeInstance: scope, postfix: "holderNameItem")
