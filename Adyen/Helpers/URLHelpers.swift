61d34cf032222c1a1f19c37d90c8965988e330ba
vladimir <vladimir.abramichev@adyen.com>
2022-07-05 10:28:55 +0200

    chore: deprecate old properties

--- Adyen/Helpers/URLHelpers.swift
+++ Adyen/Helpers/URLHelpers.swift
@@ -6,6 +6,21 @@
 
 import Foundation
 
+/// :nodoc:
+public extension URL {
+
+    @available(*, deprecated, renamed: "adyen.queryParameters", message: "Use .adyen.queryParameters instead")
+    var queryParameters: [String: String] {
+        adyen.queryParameters
+    }
+
+    @available(*, deprecated, renamed: "adyen.isHttp", message: "Use .adyen.isHttp instead")
+    var isHttp: Bool {
+        adyen.isHttp
+    }
+
+}
+
 /// So that any `URL` instance will inherit the `adyen` scope.
 /// :nodoc:
 extension URL: AdyenCompatible {}
@@ -14,6 +29,7 @@ extension URL: AdyenCompatible {}
 /// :nodoc:
 public extension AdyenScope where Base == URL {
 
+    /// An array of of query parameters
     var queryParameters: [String: String] {
         let components = URLComponents(url: base, resolvingAgainstBaseURL: true)
         let queryItems = components?.queryItems ?? []
@@ -22,8 +38,10 @@ public extension AdyenScope where Base == URL {
             ($0.name, $0.value?.removingPercentEncoding ?? "")
         })
     }
-    
+
+    /// A Boolean value indicating the URL is  http
     var isHttp: Bool {
         base.scheme == "http" || base.scheme == "https"
     }
+
 }
