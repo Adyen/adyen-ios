a43eb4fd20da12f52cf12fa00641d34ef192d94c
erenbesel <erenbesel@gmail.com>
2022-10-21 16:42:37 +0300

    chore: manually hide 4 US debit icons

--- AdyenCard/Form/FormCardNumberItem.swift
+++ AdyenCard/Form/FormCardNumberItem.swift
@@ -46,7 +46,14 @@ internal final class FormCardNumberItem: FormTextItem, AdyenObserver {
     internal init(cardTypeLogos: [FormCardLogosItem.CardTypeLogo],
                   style: FormTextItemStyle = FormTextItemStyle(),
                   localizationParameters: LocalizationParameters? = nil) {
-        self.cardTypeLogos = cardTypeLogos
+        // these 4 US debit brands are not to be displayed
+        // but should be supported so it's done here for now
+        self.cardTypeLogos = cardTypeLogos.filter { logo in
+            logo.type != .accel &&
+                logo.type != .pulse &&
+                logo.type != .star &&
+                logo.type != .nyce
+        }
         self.supportedCardTypes = cardTypeLogos.map(\.type)
         
         self.localizationParameters = localizationParameters
