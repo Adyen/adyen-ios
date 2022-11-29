f057c883813f4b57da4ab812b8d94e0082b251c7
erenbesel <erenbesel@gmail.com>
2022-11-29 15:29:23 +0100

    chore: fix xcodebuild action typos

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -19,7 +19,7 @@ xcodebuild archive -project Adyen.xcodeproj \
 -allowProvisioningUpdates \
 -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
 -authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
--authenticationKeyPath $XCODE_AUTHENTICATION_KEY_FILE_PATH
+-authenticationKeyPath $XCODE_AUTHENTICATION_KEY_PATH
 
 xcodebuild -exportArchive \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
