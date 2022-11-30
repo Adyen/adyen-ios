6c8e9000919166ae0f3a0bb23a44b81a124bf8cb
erenbesel <erenbesel@gmail.com>
2022-11-30 13:50:42 +0100

    chore: auth for both archive and exportArchive

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -17,6 +17,9 @@ xcodebuild archive -project Adyen.xcodeproj \
 -configuration Release \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
 -allowProvisioningUpdates \
+-authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
+-authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
+-authenticationKeyPath $3
 
 xcodebuild -exportArchive \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
