549e858c494e457b425ba9fc06d6e6f0c833bc97
erenbesel <erenbesel@gmail.com>
2022-11-30 13:35:22 +0100

    chore: move auth flags to exportArchive

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -17,14 +17,13 @@ xcodebuild archive -project Adyen.xcodeproj \
 -configuration Release \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
 -allowProvisioningUpdates \
--authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
--authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
--authenticationKeyPath $3
 
 xcodebuild -exportArchive \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
 -exportOptionsPlist exportOptions.plist \
 -exportPath $BUILD_PATH \
--allowProvisioningUpdates
+-allowProvisioningUpdates \
+-authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
+-authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID
 
 xcrun altool --upload-app -f $BUILD_PATH/AdyenUIHost.ipa -u $1 -p $2 --type iphoneos
