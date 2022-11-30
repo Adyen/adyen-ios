012ded8a94e80eaf14823a68faa580cc71a6a555
erenbesel <erenbesel@gmail.com>
2022-11-30 13:58:33 +0100

    chore: increase demo build for TF test

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -18,8 +18,7 @@ xcodebuild archive -project Adyen.xcodeproj \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
 -allowProvisioningUpdates \
 -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
--authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
--authenticationKeyPath $3
+-authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID 
 
 xcodebuild -exportArchive \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
@@ -27,7 +26,6 @@ xcodebuild -exportArchive \
 -exportPath $BUILD_PATH \
 -allowProvisioningUpdates \
 -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
--authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
--authenticationKeyPath $3
+-authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID
 
 xcrun altool --upload-app -f $BUILD_PATH/AdyenUIHost.ipa -u $1 -p $2 --type iphoneos
