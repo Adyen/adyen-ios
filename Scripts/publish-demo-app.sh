919c18733c0df43211241ca014b7098b5d4e00f6
erenbesel <erenbesel@gmail.com>
2022-12-01 16:02:01 +0100

    chore: merge archive and exportArchive lanes

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -2,32 +2,25 @@
 
 BUILD_PATH=Build-Temp
 
-xcodebuild clean -project Adyen.xcodeproj \
+xcodebuild clean \
+ -project Adyen.xcodeproj \
  -scheme AdyenUIHost \
- -destination="generic/platform=iOS" \
- -sdk iphoneos \
- -configuration Release
 
 mkdir -p $BUILD_PATH
 
-xcodebuild archive -project Adyen.xcodeproj \
--scheme AdyenUIHost \
--destination="generic/platform=iOS" \
--sdk iphoneos \
--configuration Release \
--archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
--allowProvisioningUpdates \
--authenticationKeyID $3 \
--authenticationKeyIssuerID $4 \
--authenticationKeyPath $5
-
-xcodebuild -exportArchive \
--archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
--exportOptionsPlist exportOptions.plist \
--exportPath $BUILD_PATH \
--allowProvisioningUpdates \
--authenticationKeyID $3 \
--authenticationKeyIssuerID $4 \
--authenticationKeyPath $5
+xcodebuild archive \
+ -project Adyen.xcodeproj \
+ -scheme AdyenUIHost \
+ -destination="generic/platform=iOS" \
+ -sdk iphoneos \
+ -configuration Release \
+ -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
+ -exportArchive \
+ -exportOptionsPlist exportOptions.plist \
+ -exportPath $BUILD_PATH \
+ -allowProvisioningUpdates \
+ -authenticationKeyID $3 \
+ -authenticationKeyIssuerID $4 \
+ -authenticationKeyPath $5
 
 xcrun altool --upload-app -f $BUILD_PATH/AdyenUIHost.ipa -u $1 -p $2 --type iphoneos
