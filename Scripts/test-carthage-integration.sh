6990398ca420c2a691f09d2981a3ed6bd60c9a75
mohamede <mohamed.eldoheiri@adyen.com>
2022-12-22 13:33:07 +0100

    fixing carthage unit tests

--- Scripts/test-carthage-integration.sh
+++ Scripts/test-carthage-integration.sh
@@ -126,6 +126,7 @@ cp "../Tests/DropIn Tests/DropInTests.swift" Tests/DropInTests.swift
 cp "../Tests/Card Tests/3DS2 Component/ThreeDS2PlusDACoreActionHandlerTests.swift" Tests/ThreeDS2PlusDACoreActionHandlerTests.swift
 cp "../Tests/Card Tests/3DS2 Component/AnyADYServiceMock.swift" Tests/AnyADYServiceMock.swift
 cp "../Tests/Card Tests/3DS2 Component/AuthenticationServiceMock.swift" Tests/AuthenticationServiceMock.swift
+cp "../Tests/Card Tests/3DS2 Component/ThreeDSResultExtension.swift" Tests/ThreeDSResultExtension.swift
 cp "../Tests/Helpers/XCTestCaseExtensions.swift" Tests/XCTestCaseExtensions.swift
 cp "../Tests/DummyData/Dummy.swift" Tests/Dummy.swift
 cp -a "../Demo/Common" Source/
