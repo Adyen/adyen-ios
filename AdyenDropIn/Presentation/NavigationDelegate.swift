6ee754f18c39959ab3f6dba9b39fb23f0e9a6a8a
vladimir <vladimir.abramichev@adyen.com>
2022-12-29 11:40:26 +0100

    fix: allow apple pay be dissmised on DropIn

---
+++ AdyenDropIn/Presentation/NavigationDelegate.swift
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2022 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Adyen
+import Foundation
+
+/// Delegates `ViewController`'s dismissal.
+internal protocol DismissalDelegate: AnyObject {
+
+    /// Asks the `delegate` to dismiss top view controller or itself as the `delegate` sees fit.
+    func dismiss(completion: (() -> Void)?)
+}
+
+internal typealias NavigationDelegate = PresentationDelegate & DismissalDelegate
