7ffa6cf3818f6ab015bf64aa7980eab295b2c3af
Vladimir Abramichev <2648655+descorp@users.noreply.github.com>
2023-02-23 15:57:17 +0100

    chore: revert to dataTask

--- Adyen/UI/Views/NetworkImageView.swift
+++ Adyen/UI/Views/NetworkImageView.swift
@@ -10,26 +10,14 @@ import UIKit
 @_spi(AdyenInternal)
 open class NetworkImageView: UIImageView {
 
+    private var dataTask: URLSessionDataTask?
+
     private enum Constants {
-        internal static let bytes10Mb: Int = 10_000_000
-        internal static let bytes1Gb: Int = 1_000_000_000
+        internal static let bytes10Mb: Int = 10000000
+        internal static let bytes1Gb: Int = 1000000000
         internal static let folderName = "AdyenIcons"
         internal static let minutes10: TimeInterval = 600
     }
-
-    private static var cache: URLCache = {
-        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-        let diskCacheURL = cachesURL.appendingPathComponent(Constants.folderName)
-        if #available(iOS 13.0, *) {
-            let cache = URLCache(memoryCapacity: Constants.bytes10Mb ,
-                                 diskCapacity: Constants.bytes1Gb,
-                                 directory: diskCacheURL)
-            cache.removeAllCachedResponses()
-            return cache
-        } else {
-            return URLCache.shared
-        }
-    }()
 
     private lazy var session: URLSession = {
         let configuration = URLSessionConfiguration.default
@@ -58,19 +46,17 @@ open class NetworkImageView: UIImageView {
         super.didMoveToWindow()
         
         // If we have an image URL and are embedded in a window, load the image if we aren't already.
-        if let imageURL = imageURL, window != nil, downloadTask == nil {
+        if let imageURL = imageURL, window != nil, dataTask == nil {
             loadImage(from: imageURL)
         }
     }
     
     // MARK: - Private
     
-    private var downloadTask: URLSessionDownloadTask?
-    
     fileprivate func setImage(_ image: UIImage) {
         DispatchQueue.main.async {
             self.image = image
-            self.downloadTask = nil
+            self.dataTask = nil
         }
     }
 
@@ -83,13 +69,12 @@ open class NetworkImageView: UIImageView {
             return setImage(image)
         }
 
-        let task = session.downloadTask(with: urlRequest) { [weak self] url, response, error in
+        let task = session.dataTask(with: urlRequest) { [weak self] data, response, error in
             guard
                 let response = response as? HTTPURLResponse,
                 response.statusCode == 200,
                 error == nil,
-                let url = url,
-                let data = try? Data(contentsOf: url),
+                let data = data,
                 let image = UIImage(data: data, scale: 1.0)
             else {
                 return
@@ -100,12 +85,30 @@ open class NetworkImageView: UIImageView {
         }
         task.resume()
         
-        downloadTask = task
+        dataTask = task
     }
     
     private func cancelCurrentTask() {
-        downloadTask?.cancel()
-        downloadTask = nil
+        dataTask?.cancel()
+        dataTask = nil
     }
     
+}
+
+extension NetworkImageView {
+
+    private static var cache: URLCache = {
+        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        let diskCacheURL = cachesURL.appendingPathComponent(Constants.folderName)
+        if #available(iOS 13.0, *) {
+            let cache = URLCache(memoryCapacity: Constants.bytes10Mb,
+                                 diskCapacity: Constants.bytes1Gb,
+                                 directory: diskCacheURL)
+            cache.removeAllCachedResponses()
+            return cache
+        } else {
+            return URLCache.shared
+        }
+    }()
+
 }
