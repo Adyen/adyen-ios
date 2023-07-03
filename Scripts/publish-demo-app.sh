cb184eae90f9434d4f15ace0443a0261017e8458
erenbesel <erenbesel@gmail.com>
2023-07-03 13:49:12 +0200

    chore: update the testflight job

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -6,7 +6,7 @@ xcodebuild clean -project Adyen.xcodeproj \
  -scheme AdyenUIHost \
  -destination="generic/platform=iOS" \
  -sdk iphoneos \
- -configuration Release \ 
+ -configuration Release \
  -skipPackagePluginValidation
 
 mkdir -p $BUILD_PATH
@@ -18,19 +18,19 @@ xcodebuild archive -project Adyen.xcodeproj \
 -configuration Release \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
 -allowProvisioningUpdates \
+-skipPackagePluginValidation \
 -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
 -authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
--authenticationKeyPath $3 \ 
--skipPackagePluginValidation
+-authenticationKeyPath $3
 
 xcodebuild -exportArchive \
 -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
 -exportOptionsPlist exportOptions.plist \
 -exportPath $BUILD_PATH \
 -allowProvisioningUpdates \
+-skipPackagePluginValidation \
 -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
 -authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
--authenticationKeyPath $3 \ 
--skipPackagePluginValidation
+-authenticationKeyPath $3
 
 xcrun altool --upload-app -f $BUILD_PATH/AdyenUIHost.ipa -u $1 -p $2 --type iphoneos
