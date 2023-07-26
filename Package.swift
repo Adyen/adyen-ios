6e295e2643d3c0fd6d7e281eaff00630c9bc237e
Robert <robertclarence.dalmeida@gmail.com>
2023-07-26 12:08:24 +0200

    Update 3ds2sdk version to 2.3.2

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
         .package(
             name: "Adyen3DS2",
             url: "https://github.com/Adyen/adyen-3ds2-ios",
-            .exact(Version(2, 3, 1))
+            .exact(Version(2, 3, 2))
         ),
         .package(
             name: "AdyenNetworking",
