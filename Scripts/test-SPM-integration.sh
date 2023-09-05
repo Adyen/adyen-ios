2b64e3a17f958bea75f4c2c3fc894766febe28cd
vladimir <vladimir.abramichev@adyen.com>
2023-09-05 12:54:03 +0200

    chore: fix SPM script

--- Scripts/test-SPM-integration.sh
+++ Scripts/test-SPM-integration.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function echo_group {
+  echo "::endgroup::"
+  echo "::group:: $1"
+}
+
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
 PROJECT_NAME=TempProject
@@ -10,24 +15,27 @@ rm -rf $PROJECT_NAME
 mkdir -p $PROJECT_NAME && cd $PROJECT_NAME
 
 # Create the package.
+echo "::group::Create the package."
+
 swift package init
 
 # Create the Package.swift.
-echo "// swift-tools-version:5.3
+echo_group "Create the Package.swift."
+echo "// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: \"TempProject\",
+    name: \"$PROJECT_NAME\",
     defaultLocalization: \"en-US\",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         .library(
-            name: \"TempProject\",
-            targets: [\"TempProject\"]
+            name: \"$PROJECT_NAME\",
+            targets: [\"$PROJECT_NAME\"]
         )
     ],
     dependencies: [
@@ -35,7 +43,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: \"TempProject\",
+            name: \"$PROJECT_NAME\",
             dependencies: [
                 .product(name: \"Adyen\", package: \"Adyen\"),
                 .product(name: \"AdyenActions\", package: \"Adyen\"),
@@ -48,23 +56,33 @@ let package = Package(
 )
 " > Package.swift
 
+# Create the package.
+echo_group "Create the package."
 swift package update
+xcodebuild -resolvePackageDependencies
 
-# Archive for generic iOS device
-echo '############# Archive for generic iOS device ###############'
-xcodebuild archive -scheme TempProject -destination 'generic/platform=iOS'
+MAX_ATTEMP=3
+ATTEMPT=0
+while [ -z $SUCCESS ] && [ "$ATTEMPT" -le "$MAX_ATTEMP" ]; do
+  xcodebuild clean -scheme $PROJECT_NAME -destination 'generic/platform=iOS' | grep -q "CLEAN SUCCEEDED" && SUCCESS=true
+  ATTEMPT=$(($ATTEMPT+1))
+done
 
 # Build for generic iOS device
-echo '############# Build for generic iOS device ###############'
-xcodebuild build -scheme TempProject -destination 'generic/platform=iOS'
+echo_group "Build for generic iOS device"
+xcodebuild build -scheme $PROJECT_NAME -destination 'generic/platform=iOS'
 
-# Archive for x86_64 simulator
-echo '############# Archive for x86_64 simulator ###############'
-xcodebuild archive -scheme TempProject -destination 'generic/platform=iOS Simulator' ARCHS=x86_64
+echo_group "Archive for generic iOS device"
+xcodebuild archive -scheme $PROJECT_NAME -destination 'generic/platform=iOS'
 
 # Build for x86_64 simulator
-echo '############# Build for x86_64 simulator ###############'
-xcodebuild build -scheme TempProject -destination 'generic/platform=iOS Simulator' ARCHS=x86_64
+echo_group "Build for x86_64 simulator"
+xcodebuild build -scheme $PROJECT_NAME -destination 'generic/platform=iOS Simulator' ARCHS=x86_64
+
+echo_group "Archive for x86_64 simulator"
+xcodebuild archive -scheme $PROJECT_NAME -destination 'generic/platform=iOS Simulator' ARCHS=x86_64
+
+echo "::endgroup::"
 
 # Clean up.
 cd ../
