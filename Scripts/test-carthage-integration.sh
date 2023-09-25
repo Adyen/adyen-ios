7b9b0025ea89955981a98a5d1ef240cc70da42aa
Alex Guretzki <goergisn@googlemail.com>
2023-09-25 11:36:35 +0200

    Fixing carthage integration script

--- Scripts/test-carthage-integration.sh
+++ Scripts/test-carthage-integration.sh
@@ -130,7 +130,7 @@ cp "../Tests/Card Tests/3DS2 Component/ThreeDSResultExtension.swift" Tests/Three
 cp "../Tests/Helpers/XCTestCase+RootViewController.swift" Tests/XCTestCase+RootViewController.swift
 cp "../Tests/Helpers/XCTestCase+Wait.swift" Tests/XCTestCase+Wait.swift
 cp "../Tests/Helpers/UIViewController+Search.swift" Tests/UIViewController+Search.swift
-cp "../Tests/Adyen\ Tests/Analytics/AnalyticsProviderMock.swift" Tests/AnalyticsProviderMock.swift
+cp "../Tests/Adyen Tests/Analytics/AnalyticsProviderMock.swift" Tests/AnalyticsProviderMock.swift
 cp "../Tests/DummyData/Dummy.swift" Tests/Dummy.swift
 cp -a "../Demo/Common" Source/
 cp -a "../Demo/UIKit" Source/
