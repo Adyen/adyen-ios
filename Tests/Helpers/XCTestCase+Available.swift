a1325a57d37bb4bb7c93f11460522ea585034172
Alex Guretzki <goergisn@googlemail.com>
2023-09-28 15:31:52 +0200

    re-added xctestcase+available

---
+++ Tests/Helpers/XCTestCase+Available.swift
@@ -0,0 +1,52 @@
+//
+// Copyright (c) 2021 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import XCTest
+
+extension XCTestCase {
+
+    enum Available {
+        static var iOS12: Bool {
+            if #available(iOS 12.0, *) {
+                return true
+            } else {
+                return false
+            }
+        }
+
+        static var iOS13: Bool {
+            if #available(iOS 13.0, *) {
+                return true
+            } else {
+                return false
+            }
+        }
+
+        static var iOS15: Bool {
+            if #available(iOS 15.0, *) {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        static var iOS16: Bool {
+            if #available(iOS 16.0, *) {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        static var iOS17: Bool {
+            if #available(iOS 17.0, *) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
