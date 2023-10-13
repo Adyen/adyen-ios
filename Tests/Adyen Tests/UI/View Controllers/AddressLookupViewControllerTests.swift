7a53faca4db9a79a60e964afa92dcc830f892f9e
Alex Guretzki <goergisn@googlemail.com>
2023-10-13 14:40:53 +0200

    Update AddressLookupViewControllerTests.swift

--- Tests/Adyen Tests/UI/View Controllers/AddressLookupViewControllerTests.swift
+++ Tests/Adyen Tests/UI/View Controllers/AddressLookupViewControllerTests.swift
@@ -227,9 +227,9 @@ class AddressLookupViewControllerTests: XCTestCase {
         let resultHandler: ([ListItem]) -> Void = {
             // We don't have a ListViewController that provides the loadingHandler
             // so we provide one here which also allows us to test if it's called correctly
-            $0[1].loadingHandler = { _ in }//loadingExpectation.fulfill() }
+//            $0[1].loadingHandler = { _ in }//loadingExpectation.fulfill() }
             // Selecting the 2nd item in the list as the first one is the manual input cell
-//            $0[1].selectionHandler?()
+            $0[1].selectionHandler?()
         }
         
         resultHandler([.init(title: ""), .init(title: "")])
