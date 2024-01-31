b60a22d9bab34c2802d4e35efb42cc59179abad1
Alex Guretzki <goergisn@googlemail.com>
2024-01-31 13:16:43 +0100

    Updated BundleSPMExtension

--- Adyen/Utilities/BundleSPMExtension.swift
+++ Adyen/Utilities/BundleSPMExtension.swift
@@ -11,7 +11,7 @@ import Foundation
 /// since swift packages has different code to access internal resources,
 /// that doesn't compile in a normal xcode project.
 /// The Bundle extension in `BundleExtension.swift` is used instead.
-@_spi(AdyenInternal)
+@_documentation(visibility: internal)
 extension Bundle {
 
     /// The main bundle of the framework.
