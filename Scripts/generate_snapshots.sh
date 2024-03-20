13de4407041d8e42bfbf3306028144a458fb671b
Alex Guretzki <goergisn@googlemail.com>
2024-03-20 14:15:19 +0100

    Adding github workflow

--- Scripts/generate_snapshots.sh
+++ Scripts/generate_snapshots.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-find . -type d -name "__Snapshots__"
+# Deleting all existing snapshots
+echo "🗑️  Deleting existing Snapshots"
+find ./UITests -name "__Snapshots__" -exec rm -rf {} +
+
+# Generating snapshots for iPhone
+echo "🤳 Generating Snapshots"
+xcodebuild test -project Adyen.xcodeproj -scheme 'GenerateSnapshots' -destination "name=iPhone 15 Pro,OS=17.4" -destination "name=iPad Pro (12.9-inch) (6th generation),OS=17.4"
