6f30a4727749e3ce69df4f520cdb14d7be044134
Alex Guretzki <goergisn@googlemail.com>
2024-04-26 13:56:29 +0200

    Adding header

--- AdyenWeChatPay/WeChatPayActionComponent/WeChatPayActionComponent.swift
+++ AdyenWeChatPay/WeChatPayActionComponent/WeChatPayActionComponent.swift
@@ -11,9 +11,7 @@
 @_spi(AdyenInternal) import Adyen
 import Foundation
 
-#if !targetEnvironment(simulator) && canImport(AdyenWeChatPay)
-
-    import AdyenWeChatPay
+#if !targetEnvironment(simulator)
 
     /// Action component to handle WeChat Pay SDK action.
     public final class WeChatPaySDKActionComponent: NSObject, AnyWeChatPaySDKActionComponent {
