2aaabe85ca464f2f1f321a469e40c225ab2895bd
Andrii Mamchenko <atmamont@gmail.com>
2024-04-29 11:53:51 +0200

    Revert "Replace WeChatPay custom framework with WeChatPay xcframework"

--- Package.swift
+++ Package.swift
@@ -72,6 +72,11 @@ let package = Package(
             .exact(Version(2, 0, 0))
         ),
         .package(
+            name: "AdyenWeChatPayInternal",
+            url: "https://github.com/Adyen/adyen-wechatpay-ios",
+            .exact(Version(2, 1, 0))
+        ),
+        .package(
             name: "PayKit",
             url: "https://github.com/cashapp/cash-app-pay-ios-sdk",
             .exact(Version(0, 5, 1))
@@ -162,14 +167,10 @@ let package = Package(
         .target(
             name: "AdyenWeChatPay",
             dependencies: [
-                .target(name: "AdyenActions"),
-                .target(name: "WeChatPaySDK")
+                .product(name: "AdyenWeChatPayInternal", package: "AdyenWeChatPayInternal"),
+                .target(name: "AdyenActions")
             ],
             path: "AdyenWeChatPay/WeChatPayActionComponent"
-        ),
-        .binaryTarget(
-            name: "WeChatPaySDK",
-            path: "XCFramework/WeChatPay/WechatOpenSDK-XCFramework.xcframework"
         ),
         .target(
             name: "AdyenCashAppPay",
