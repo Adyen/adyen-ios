afcdd9852531fb0e9d3dbf3344fb838375a1ab1b
Alex Guretzki <goergisn@googlemail.com>
2024-04-30 15:38:16 +0200

    Commenting out archiving code

--- Scripts/test-SPM-integration.sh
+++ Scripts/test-SPM-integration.sh
@@ -42,19 +42,18 @@ let package = Package(
                 .product(name: \"AdyenCard\", package: \"Adyen\"),
                 .product(name: \"AdyenComponents\", package: \"Adyen\"),
                 .product(name: \"AdyenSession\", package: \"Adyen\"),
-                .product(name: \"AdyenDropIn\", package: \"Adyen\"),
                 .product(name: \"AdyenWeChatPay\", package: \"Adyen\"),
                 .product(name: \"AdyenSwiftUI\", package: \"Adyen\"),
                 .product(name: \"AdyenCashAppPay\", package: \"Adyen\"),
                 .product(name: \"AdyenTwint\", package: \"Adyen\"),
-                .product(name: \"AdyenDelegatedAuthentication\", package: \"Adyen\")
+                .product(name: \"AdyenDelegatedAuthentication\", package: \"Adyen\"),
+                .product(name: \"AdyenDropIn\", package: \"Adyen\")
             ]
         )
     ]
 )
 " > Package.swift
 
-swift package reset
 swift package update
 
 # This is a hack to work around a bug with SPM
@@ -62,17 +61,17 @@ swift package update
 swift package dump-pif > /dev/null || true
 xcodebuild clean -scheme TempProject -destination 'generic/platform=iOS' > /dev/null || true
 
-# Archive for generic iOS device
-echo '############# Archive for generic iOS device ###############'
-xcodebuild archive -scheme TempProject -destination 'generic/platform=iOS' -skipPackagePluginValidation
+## Archive for generic iOS device
+#echo '############# Archive for generic iOS device ###############'
+#xcodebuild archive -scheme TempProject -destination 'generic/platform=iOS' -skipPackagePluginValidation
 
 # Build for generic iOS device
 echo '############# Build for generic iOS device ###############'
 xcodebuild build -scheme TempProject -destination 'generic/platform=iOS' -skipPackagePluginValidation
 
-# Archive for x86_64 simulator
-echo '############# Archive for x86_64 simulator ###############'
-xcodebuild archive -scheme TempProject -destination 'generic/platform=iOS Simulator' ARCHS=x86_64 -skipPackagePluginValidation
+## Archive for x86_64 simulator
+#echo '############# Archive for x86_64 simulator ###############'
+#xcodebuild archive -scheme TempProject -destination 'generic/platform=iOS Simulator' ARCHS=x86_64 -skipPackagePluginValidation
 
 # Build for x86_64 simulator
 echo '############# Build for x86_64 simulator ###############'
