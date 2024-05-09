4ff6a62e9ef6566746abdc98888da5ccd95d738f
Naufal Aros <naufal.aros@adyen.com>
2024-05-09 17:03:23 -0500

    Use card component within non scrollable container view

---
+++ Demo/UIKit/CustomComponentViewController.swift
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2024 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import UIKit
+
+class CustomComponentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
