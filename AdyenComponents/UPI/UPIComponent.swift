23520648c7d29758e8ebeadaaf45602c32cc811b
Neelam Sharma <neelam.sharma@adyen.com>
2024-06-04 10:56:31 +0200

    Added localisation

--- AdyenComponents/UPI/UPIComponent.swift
+++ AdyenComponents/UPI/UPIComponent.swift
@@ -49,7 +49,7 @@ public final class UPIComponent: PaymentComponent,
     /// The delegate of the component.
     public weak var delegate: PaymentComponentDelegate?
 
-    private var currentSelectedItem: SelectableFormItem?
+    internal var currentSelectedItem: SelectableFormItem?
 
     private var previousSelectedItem: SelectableFormItem?
 
@@ -93,8 +93,8 @@ public final class UPIComponent: PaymentComponent,
 
     /// The upi based payment instructions label item.
     internal lazy var instructionsLabelItem: FormLabelItem = {
-        // TODO: Add localisation
-        let item = FormLabelItem(text: "How would you like to use UPI?",
+        let item = FormLabelItem(text: localizedString(.upiModeSelection,
+                                                       configuration.localizationParameters),
                                  style: configuration.style.footnoteLabel)
         item.style.textAlignment = .left
         item.identifier = ViewIdentifierBuilder.build(scopeInstance: self,
@@ -104,13 +104,20 @@ public final class UPIComponent: PaymentComponent,
 
     /// The upi selection segment control item to choose the upi flow.
     internal lazy var upiFlowSelectionItem: FormSegmentedControlItem = {
-        // TODO: Add localisation
-        let item = FormSegmentedControlItem(items: ["Pay by any UPI app", "Other UPI options"],
-                                            style: configuration.style.segmentedControlStyle,
-                                            identifier: ViewIdentifierBuilder.build(
-                                                scopeInstance: self,
-                                                postfix: ViewIdentifier.upiFlowSelectionItem
-                                            ))
+        let item = FormSegmentedControlItem(
+            items: [
+                localizedString(.UPIFirstTabTitle,
+                                configuration.localizationParameters),
+                localizedString(.UPISecondTabTitle,
+                                configuration.localizationParameters)
+            ],
+            style: configuration.style.segmentedControlStyle,
+            identifier: ViewIdentifierBuilder.build(
+                scopeInstance: self,
+                postfix: ViewIdentifier.upiFlowSelectionItem
+            )
+        )
+
         item.identifier = ViewIdentifierBuilder.build(scopeInstance: self,
                                                       postfix: ViewIdentifier.upiFlowSelectionItem)
         item.selectionHandler = { [weak self] changedIndex in
@@ -122,8 +129,8 @@ public final class UPIComponent: PaymentComponent,
     /// The  virtual payment address text input item.
     internal lazy var virtualPaymentAddressItem: FormTextInputItem = {
         let item = FormTextInputItem(style: configuration.style.textField)
-        // TODO: Add localised string
-        item.title = "Enter UPI ID / VPA"
+        item.title = localizedString(.UPICollectFieldLabel,
+                                     configuration.localizationParameters)
         item.validator = LengthValidator(minimumLength: 1)
         item.validationFailureMessage = localizedString(.UPIVpaValidationMessage, configuration.localizationParameters)
         item.identifier = ViewIdentifierBuilder.build(scopeInstance: self,
@@ -144,7 +151,8 @@ public final class UPIComponent: PaymentComponent,
     /// The UPI enter UPI/VPA list item.
     internal lazy var vpaItem: SelectableFormItem = {
         let selectableItem = SelectableFormItem(
-            title: "Enter UPI ID",
+            title: localizedString(.UPICollectDropdownLabel,
+                                   configuration.localizationParameters),
             imageUrl: nil,
             isSelected: false,
             style: .init(),
@@ -165,7 +173,8 @@ public final class UPIComponent: PaymentComponent,
 
     /// The QRCode generation message item.
     internal lazy var qrCodeGenerationLabelContainerItem: FormContainerItem = {
-        let item = FormLabelItem(text: localizedString(.UPIQrcodeGenerationMessage, configuration.localizationParameters),
+        let item = FormLabelItem(text: localizedString(.UPIQrcodeGenerationMessage,
+                                                       configuration.localizationParameters),
                                  style: configuration.style.footnoteLabel)
         item.style.textAlignment = .center
         item.identifier = ViewIdentifierBuilder.build(scopeInstance: self,
@@ -241,11 +250,9 @@ public final class UPIComponent: PaymentComponent,
             for item in upiAppsList {
                 formViewController.append(item)
             }
-            formViewController.append(virtualPaymentAddressItem)
             virtualPaymentAddressItem.isHidden.wrappedValue = true
-        } else {
-            formViewController.append(virtualPaymentAddressItem)
         }
+        formViewController.append(virtualPaymentAddressItem)
         formViewController.append(FormSpacerItem(numberOfSpaces: 2))
         formViewController.append(continueButton)
 
