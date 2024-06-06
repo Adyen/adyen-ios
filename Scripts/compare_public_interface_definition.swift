752515e23af33705f01ae6687486794682a0a13f
Alex Guretzki <goergisn@googlemail.com>
2024-06-06 16:25:46 +0200

    Adding scripts to detect api changes

---
+++ Scripts/compare_public_interface_definition.swift
@@ -0,0 +1,205 @@
+#!/usr/bin/env xcrun swift
+
+//
+// Copyright (c) 2024 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Foundation
+
+let currentDirectory = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+
+let old = CommandLine.arguments[1]
+let new = CommandLine.arguments[2]
+
+class Element: Codable, Equatable, CustomDebugStringConvertible {
+    let kind: String
+    let name: String
+    let mangledName: String?
+    let printedName: String
+    let declKind: String?
+    
+    let children: [Element]?
+    let spiGroupNames: [String]?
+    
+    var parent: Element?
+    
+    enum CodingKeys: String, CodingKey {
+        case kind
+        case name
+        case printedName
+        case mangledName
+        case children
+        case spiGroupNames = "spi_group_names"
+        case declKind
+    }
+    
+    var debugDescription: String {
+        var definition = ""
+        spiGroupNames?.forEach {
+            definition += "@_spi(\($0)) "
+        }
+        definition += "public "
+        
+        if declKind == "Var" {
+            definition += "var "
+        } else if declKind == "Func" {
+            definition += "func "
+        } else if declKind == "Import" {
+            definition += "import"
+        }
+        
+        definition += "\(printedName)"
+        
+        return definition
+    }
+    
+    public static func == (lhs: Element, rhs: Element) -> Bool {
+        lhs.mangledName == rhs.mangledName
+        && lhs.children == rhs.children
+        && lhs.spiGroupNames == rhs.spiGroupNames
+    }
+    
+    var parentPath: String {
+        var parent = self.parent
+        var path = [parent?.name]
+        while parent != nil {
+            parent = parent?.parent
+            path += [parent?.name]
+        }
+        
+        var sanitizedPath = path.compactMap({$0})
+        
+        if sanitizedPath.last == "TopLevel" {
+            sanitizedPath.removeLast()
+        }
+        
+        return sanitizedPath.reversed().joined(separator: ".")
+    }
+}
+
+class Definition: Codable, Equatable {
+    let root: Element
+    
+    enum CodingKeys: String, CodingKey {
+        case root = "ABIRoot"
+    }
+    
+    public static func == (lhs: Definition, rhs: Definition) -> Bool {
+        lhs.root == rhs.root
+    }
+}
+
+struct Change {
+    enum ChangeType {
+        case addition
+        case removal
+        case change
+        
+        var icon: String {
+            switch self {
+            case .addition:
+                return "🐣"
+            case .removal:
+                return "💔"
+            case .change:
+                return "🔀"
+            }
+        }
+    }
+    
+    var changeType: ChangeType
+    var parentName: String
+    var changeDescription: String
+}
+
+func recursiveCompare(element lhs: Element, to rhs: Element, oldFirst: Bool) -> [Change] {
+    if lhs == rhs {
+        return []
+    }
+    
+    var changes = [Change]()
+    
+    if oldFirst, lhs.printedName != rhs.printedName {
+        changes += [.init(changeType: .change, parentName: lhs.parentPath, changeDescription: "\(lhs) ➡️  \(rhs)")]
+    }
+    
+    if oldFirst, lhs.spiGroupNames != rhs.spiGroupNames {
+        changes += [.init(changeType: .change, parentName: lhs.parentPath, changeDescription: "\(lhs) ➡️  \(rhs)")]
+    }
+    
+    if oldFirst, lhs.children == rhs.children {
+        changes += [.init(changeType: .change, parentName: lhs.parentPath, changeDescription: "\(lhs) ➡️  \(rhs)")]
+    }
+    
+    changes += lhs.children?.flatMap { lhsElement in
+        if let rhsChildForName = rhs.children?.first(where: { ($0.mangledName ?? $0.name) == (lhsElement.mangledName ?? lhsElement.name) }) {
+            return recursiveCompare(element: lhsElement, to: rhsChildForName, oldFirst: oldFirst)
+        } else {
+            if oldFirst {
+                return [.init(changeType: .removal, parentName: lhsElement.parentPath, changeDescription: "\(lhsElement) was removed")]
+            } else {
+                return [.init(changeType: .addition, parentName: lhsElement.parentPath, changeDescription: "\(lhsElement) was added")]
+            }
+        }
+    } ?? []
+    
+    return changes
+}
+
+func setupRelationships(for element: Element, parent: Element?) {
+    element.children?.forEach {
+        $0.parent = element
+        setupRelationships(for: $0, parent: element)
+    }
+}
+
+func compare() throws -> Bool {
+    let oldFileUrl = currentDirectory.appending(path: old)
+    let newFileUrl = currentDirectory.appending(path: new)
+    
+    //print("Old:", oldFileUrl)
+    //print("New:", newFileUrl)
+    
+    let decodedOldDefinition = try JSONDecoder().decode(
+        Definition.self,
+        from: Data(contentsOf: oldFileUrl)
+    )
+    
+    let decodedNewDefinition = try JSONDecoder().decode(
+        Definition.self,
+        from: Data(contentsOf: newFileUrl)
+    )
+    
+    if decodedOldDefinition == decodedNewDefinition {
+        return true
+    }
+    
+    setupRelationships(for: decodedOldDefinition.root, parent: nil)
+    setupRelationships(for: decodedNewDefinition.root, parent: nil)
+    
+    let changes = recursiveCompare(element: decodedOldDefinition.root, to: decodedNewDefinition.root, oldFirst: true) + recursiveCompare(element: decodedNewDefinition.root, to: decodedOldDefinition.root, oldFirst: false)
+    
+    var groupedChanges = [String: [Change]]()
+    
+    changes.forEach {
+        groupedChanges[$0.parentName] = (groupedChanges[$0.parentName] ?? []) + [$0]
+    }
+    
+    
+    groupedChanges.keys.sorted().forEach { key in
+        print("[\(key)]")
+        groupedChanges[key]?.forEach {
+            print("- \($0.changeType.icon) \($0.changeDescription)")
+        }
+    }
+    
+    return false
+}
+
+do {
+    print(try compare() ? "✅ Both files are equal" : "🔀 Changes detected")
+} catch {
+    print(error)
+}
---
+++ Scripts/generate_public_interface_definition.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "Branch:" $1
+echo "Repo:" $2
+
+rm -rf .build
+mv Adyen.xcodeproj Adyen.xcode_proj
+rm -rf comparison_version
+
+function cleanup() {
+    rm -rf .build
+    rm -rf comparison_version
+    mv Adyen.xcode_proj Adyen.xcodeproj
+}
+
+trap cleanup EXIT
+
+echo "↘️  Checking out comparison version"
+mkdir comparison_version
+cd comparison_version
+# ${{ github.server_url }}/${{ github.repository }} # https://github.com/Adyen/adyen-ios.git
+# ${{ github.event.pull_request.base.ref }} # e.g. develop
+git clone -b $1 $2
+cd adyen-ios
+mv Adyen.xcodeproj Adyen.xcode_proj
+
+echo "👷 Building comparison project"
+xcodebuild -derivedDataPath .build -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -scheme "AdyenDropIn" -destination "platform=iOS,name=Any iOS Device" -target "x86_64-apple-ios16.4-simulator" -quiet
+
+echo "📋 Generating comparison api_dump"
+xcrun swift-api-digester -dump-sdk -module AdyenDropIn -o ../../api_dump_comparison.json -I .build/Build/Products/Debug-iphonesimulator -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -target "arm64-apple-ios17.2-simulator"
+xcrun swift-api-digester -dump-sdk -module Adyen -o ../../api_dump_comparison.json -I .build/Build/Products/Debug-iphonesimulator -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -target "arm64-apple-ios17.2-simulator"
+
+cd ../..
+
+# Building project in `.build` directory
+echo "👷 Building updated project"
+xcodebuild -derivedDataPath .build -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -scheme "AdyenDropIn" -destination "platform=iOS,name=Any iOS Device" -target "x86_64-apple-ios16.4-simulator" -quiet
+
+# Generating api_dump
+echo "📋 Generating new api_dump"
+xcrun swift-api-digester -dump-sdk -module AdyenDropIn -o api_dump.json -I .build/Build/Products/Debug-iphonesimulator -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -target "arm64-apple-ios17.2-simulator"
+xcrun swift-api-digester -dump-sdk -module Adyen -o api_dump.json -I .build/Build/Products/Debug-iphonesimulator -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -target "arm64-apple-ios17.2-simulator"
+
+echo "🔀 Comparing"
+
+echo "- diff"
+diff api_dump_comparison.json api_dump.json
+
+echo "- swift-api-digester -diagnose-sdk"
+# This command does not take @_spi into account unfortunately (It works well with changing from public to private or add new func/var/objects)
+# xcrun swift-api-digester -diagnose-sdk -module Adyen -o api_diff -use-interface-for-module Adyen -abi -BI ./last_release/adyen-ios/.build/Build/Products/Debug-iphonesimulator -I .build/Build/Products/Debug-iphonesimulator -bsdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" -target "arm64-apple-ios17.2-simulator"
+# cat api_diff
+
+# This command also does not take @_spi into account (Same as swift-api-digester -diagnose-sdk basically but running on the json files)
+xcrun swift-api-digester -diagnose-sdk --input-paths api_dump_comparison.json -input-paths api_dump.json
