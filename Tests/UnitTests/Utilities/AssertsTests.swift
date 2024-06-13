c84f3a0a1374e54c73432046e57efb68b47c7e95
Naufal Aros <naufal.aros@adyen.com>
2024-06-13 11:15:35 +0200

    Update compile error in tests suite

--- Tests/UnitTests/Utilities/AssertsTests.swift
+++ Tests/UnitTests/Utilities/AssertsTests.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2024 Adyen N.V.
+// Copyright (c) 2021 Adyen N.V.
 //
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
@@ -48,7 +48,11 @@ class AssertsTests: XCTestCase {
     }
 
     func testFormViewControllerPreferredContentSizeAssertion() {
-        let sut = FormViewController(style: FormComponentStyle(), localizationParameters: nil)
+        let sut = FormViewController(
+            scrollEnabled: true,
+            style: FormComponentStyle(),
+            localizationParameters: nil
+        )
         let expectation = XCTestExpectation(description: "Dummy Expectation")
 
         AdyenAssertion.listener = { message in
