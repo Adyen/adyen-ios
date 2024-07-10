231678a6d4bb13069569503c0339faa56ad9247b
Alex Guretzki <goergisn@googlemail.com>
2024-07-10 15:45:56 +0200

    [SwiftPackageAPIDiff] Adding Initial Tests (#1727)
    ## Summary
    - Adding tests

--- Scripts/public-api-diff/Sources/Helpers/SDKAnalyzer/SDKAnalyzer.swift
+++ Scripts/public-api-diff/Sources/Helpers/SDKAnalyzer/SDKAnalyzer.swift
@@ -8,20 +8,26 @@ import Foundation
 
 // MARK: - Model
 
-enum SDKAnalyzer {
+struct SDKAnalyzer {
     
-    public static func analyze(
+    private let fileHandler: FileHandling
+    private let xcodeTools: XcodeTools
+    
+    init(fileHandler: FileHandling, xcodeTools: XcodeTools) {
+        self.fileHandler = fileHandler
+        self.xcodeTools = xcodeTools
+    }
+    
+    public func analyze(
         old oldProjectDirectoryPath: String,
-        new newProjectDirectoryPath: String,
-        fileHandler: FileHandling,
-        xcodeTools: XcodeTools
+        new newProjectDirectoryPath: String
     ) throws -> [String: [SDKAnalyzer.Change]] {
         
         print("🔍 Scanning for products changes")
         
-        let packageFileChanges = try SDKAnalyzer.analyzePackageFile(
-            updated: PackageFileHelper.packagePath(for: newProjectDirectoryPath),
-            to: PackageFileHelper.packagePath(for: oldProjectDirectoryPath),
+        let packageFileChanges = try Self.analyzePackageFile(
+            new: PackageFileHelper.packagePath(for: newProjectDirectoryPath),
+            old: PackageFileHelper.packagePath(for: oldProjectDirectoryPath),
             fileHandler: fileHandler
         )
         
@@ -45,7 +51,7 @@ enum SDKAnalyzer {
             xcodeTools: xcodeTools
         )
         
-        var changesPerTarget = try SDKAnalyzer.analyzeSdkDump(
+        var changesPerTarget = try Self.analyzeSdkDumps(
             for: allTargets,
             newDumpGenerator: newDumpGenerator,
             oldDumpGenerator: oldDumpGenerator
@@ -58,46 +64,35 @@ enum SDKAnalyzer {
         
         return changesPerTarget
     }
+}
+
+// MARK: - Private
+
+private extension SDKAnalyzer {
     
-    private static func analyzePackageFile(
-        updated updatedPackageFilePath: String,
-        to comparisonPackageFilePath: String,
+    static func analyzePackageFile(
+        new newPackageFilePath: String,
+        old oldPackageFilePath: String,
         fileHandler: FileHandling
     ) throws -> [Change] {
-        var libraryChanges = [Change]()
         
         let oldProducts = try PackageFileHelper(
-            packagePath: comparisonPackageFilePath,
+            packagePath: oldPackageFilePath,
             fileHandler: fileHandler
         ).availableProducts()
         
         let newProducts = try PackageFileHelper(
-            packagePath: updatedPackageFilePath,
+            packagePath: newPackageFilePath,
             fileHandler: fileHandler
         ).availableProducts()
         
-        let removedLibaries = oldProducts.subtracting(newProducts)
-        libraryChanges += removedLibaries.map {
-            .init(
-                changeType: .removal,
-                parentName: "",
-                changeDescription: "`.library(name: \"\($0)\", ...)` was removed"
-            )
-        }
-        
-        let addedLibraries = newProducts.subtracting(oldProducts)
-        libraryChanges += addedLibraries.map {
-            .init(
-                changeType: .addition,
-                parentName: "",
-                changeDescription: "`.library(name: \"\($0)\", ...)` was added"
-            )
-        }
-        
-        return libraryChanges
+        return PackageAnalyzer.analyzeProductDifferences(
+            new: newProducts,
+            old: oldProducts
+        )
     }
     
-    private static func analyzeSdkDump(
+    private static func analyzeSdkDumps(
         for allTargets: [String],
         newDumpGenerator: SDKDumpGenerator,
         oldDumpGenerator: SDKDumpGenerator
@@ -105,92 +100,33 @@ enum SDKAnalyzer {
         
         var changesPerTarget = [String: [SDKAnalyzer.Change]]() // [ModuleName: [SDKDumpAnalyzer.Change]]
         
-        try allTargets.forEach { targetName in
-            let diff = try SDKAnalyzer.diffSdkDump(
-                newDump: newDumpGenerator.generate(for: targetName),
-                oldDump: oldDumpGenerator.generate(for: targetName)
+        allTargets.forEach { targetName in
+            
+            var newDump: SDKDump? = nil
+            var oldDump: SDKDump? = nil
+            
+            print("🕵️‍♂️ Analyzing `\(targetName)`")
+            
+            do {
+                oldDump = try oldDumpGenerator.generate(for: targetName)
+            } catch {
+                print("OLD", targetName, error)
+            }
+            
+            do {
+                newDump = try newDumpGenerator.generate(for: targetName)
+            } catch {
+                print("NEW", targetName, error)
+            }
+            
+            let diff = SDKDumpAnalyzer.analyzeSdkDump(
+                newDump: newDump,
+                oldDump: oldDump
             )
             
             if !diff.isEmpty { changesPerTarget[targetName] = diff }
         }
         
         return changesPerTarget
-    }
-    
-    private static func diffSdkDump(
-        newDump: SDKDump?,
-        oldDump: SDKDump?
-    ) throws -> [Change] {
-        
-        guard newDump != oldDump else { return [] }
-        
-        guard let newDump else {
-            return [.init(changeType: .removal, parentName: "", changeDescription: "Target was removed")]
-        }
-        
-        guard let oldDump else {
-            return [.init(changeType: .addition, parentName: "", changeDescription: "Target was added")]
-        }
-        
-        setupRelationships(for: newDump.root, parent: nil)
-        setupRelationships(for: oldDump.root, parent: nil)
-        
-        return recursiveCompare(
-            element: oldDump.root,
-            to: newDump.root,
-            oldFirst: true
-        ) + recursiveCompare(
-            element: newDump.root,
-            to: oldDump.root,
-            oldFirst: false
-        )
-    }
-}
-
-// MARK: - Private
-
-private extension SDKAnalyzer {
-    
-    static func setupRelationships(for element: SDKDump.Element, parent: SDKDump.Element?) {
-        element.children?.forEach {
-            $0.parent = element
-            setupRelationships(for: $0, parent: element)
-        }
-    }
-    
-    static func recursiveCompare(element lhs: SDKDump.Element, to rhs: SDKDump.Element, oldFirst: Bool) -> [Change] {
-        if lhs == rhs { return [] }
-        
-        if lhs.isSpiInternal, rhs.isSpiInternal {
-            // If both elements are spi internal we can ignore them as they are not in the public interface
-            return []
-        }
-        
-        var changes = [Change]()
-        
-        if oldFirst, lhs.definition != rhs.definition {
-            // TODO: Show what exactly changed (name, spi, conformance, declAttributes, ...) as a bullet list maybe (add a `changeList` property to `Change`)
-            changes += [.init(changeType: .change, parentName: lhs.parentPath, changeDescription: "`\(lhs)`\n  ➡️  `\(rhs)`")]
-        }
-        
-        changes += lhs.children?.flatMap { lhsElement in
-            if let rhsChildForName = rhs.children?.first(where: { $0.printedName == lhsElement.printedName }) {
-                return recursiveCompare(element: lhsElement, to: rhsChildForName, oldFirst: oldFirst)
-            } else {
-                // Type changes we handle as a change, not an addition/removal (they are in the children array tho)
-                if lhsElement.isTypeInformation { return [] }
-                
-                // An spi-internal element was added/removed which we do not count as a public change
-                if lhsElement.isSpiInternal { return [] }
-                
-                if oldFirst {
-                    return [.init(changeType: .removal, parentName: lhsElement.parentPath, changeDescription: "`\(lhsElement)` was removed")]
-                } else {
-                    return [.init(changeType: .addition, parentName: lhsElement.parentPath, changeDescription: "`\(lhsElement)` was added")]
-                }
-            }
-        } ?? []
-        
-        return changes
     }
 }
---
+++ Scripts/public-api-diff/Tests/PublicApiDiffTests/SDKAnalyzerTests.swift
@@ -0,0 +1,114 @@
+//
+// Copyright (c) 2024 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+@testable import public_api_diff
+import XCTest
+
+class SDKAnalyzerTests: XCTestCase {
+    
+    func test_analyze_noChanges() throws {
+        let mockShell = MockShell { command in "" }
+        
+        var mockFileHandler = MockFileHandler()
+        mockFileHandler.handleLoadData = { path in
+            let packageName = path.components(separatedBy: "/").first
+            let resourcePath = try XCTUnwrap(Bundle.module.path(forResource: packageName, ofType: "txt"))
+            return try XCTUnwrap(FileManager.default.contents(atPath: resourcePath))
+        }
+        mockFileHandler.handleFileExists = { _ in
+            return true
+        }
+        
+        let xcodeTools = XcodeTools(shell: mockShell)
+        
+        let analyzer = SDKAnalyzer(
+            fileHandler: mockFileHandler,
+            xcodeTools: xcodeTools
+        )
+        
+        let expectedChanges: [String: [SDKAnalyzer.Change]] = [:]
+        
+        let changes = try analyzer.analyze(
+            old: "NewPackage",
+            new: "NewPackage"
+        )
+        
+        XCTAssertEqual(changes, expectedChanges)
+    }
+    
+    func test_analyze_targetChanges() throws {
+        
+        let mockShell = MockShell { _ in
+            return ""
+        }
+        
+        var mockFileHandler = MockFileHandler()
+        mockFileHandler.handleLoadData = { path in
+            let packageName = path.components(separatedBy: "/").first
+            let resourcePath = try XCTUnwrap(Bundle.module.path(forResource: packageName, ofType: "txt"))
+            return try XCTUnwrap(FileManager.default.contents(atPath: resourcePath))
+        }
+        mockFileHandler.handleFileExists = { _ in
+            return true
+        }
+        
+        let xcodeTools = XcodeTools(shell: mockShell)
+        
+        let analyzer = SDKAnalyzer(
+            fileHandler: mockFileHandler,
+            xcodeTools: xcodeTools
+        )
+        
+        let expectedChanges: [String: [SDKAnalyzer.Change]] = [
+            "Package.swift": [
+                .init(changeType: .removal, parentName: "", changeDescription: "`.library(name: \"OldLibrary\", ...)` was removed"),
+                .init(changeType: .addition, parentName: "", changeDescription: "`.library(name: \"NewLibrary\", ...)` was added")
+            ]
+        ]
+        
+        let changes = try analyzer.analyze(
+            old: "OldPackage",
+            new: "NewPackage"
+        )
+        
+        XCTAssertEqual(changes, expectedChanges)
+    }
+    
+    func test_analyze_codeChanges() throws {
+        
+        // TODO: Implement
+        
+        let mockShell = MockShell { _ in
+            return ""
+        }
+        
+        var mockFileHandler = MockFileHandler()
+        mockFileHandler.handleLoadData = { path in
+            let packageName = path.components(separatedBy: "/").first
+            let resourcePath = try XCTUnwrap(Bundle.module.path(forResource: packageName, ofType: "txt"))
+            return try XCTUnwrap(FileManager.default.contents(atPath: resourcePath))
+        }
+        mockFileHandler.handleFileExists = { _ in
+            return true
+        }
+        
+        let xcodeTools = XcodeTools(shell: mockShell)
+        
+        let analyzer = SDKAnalyzer(
+            fileHandler: mockFileHandler,
+            xcodeTools: xcodeTools
+        )
+        
+        let expectedChanges: [String: [SDKAnalyzer.Change]] = [:]
+        
+        let changes = try analyzer.analyze(
+            old: "NewPackage",
+            new: "NewPackage"
+        )
+        
+        XCTAssertEqual(changes, expectedChanges)
+    }
+}
