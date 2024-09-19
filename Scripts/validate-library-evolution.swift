1e0eb34cdcde016a2bbdee9246f86ba4a87166e7
Alex Guretzki <alexander.guretzki@adyen.com>
2024-09-19 13:58:42 +0200

    Refactoring validation script

--- Scripts/validate-library-evolution.swift
+++ Scripts/validate-library-evolution.swift
@@ -2,6 +2,14 @@
 
 import Foundation
 
+/**
+ This script builds a custom swift package containing all frameworks inside of the xcarchive passed when launching.
+ 
+ Input parameters:
+  - CommandLine.arguments[1]: Path to the .xcarchive that contains all the frameworks that should be tested on library-evolution support
+  - CommandLine.arguments[2]: Name of the project that allows testing (Has to be a name, not a path as it's reused as the scheme of the swift package)
+ */
+
 let fileManager = FileManager.default
 let currentDirectoryURL = URL(fileURLWithPath: fileManager.currentDirectoryPath, isDirectory: true)
 
@@ -10,6 +18,8 @@ let outputProjectURL = URL(fileURLWithPath: CommandLine.arguments[2], relativeTo
 
 let dSYMsDirectoryURL = xcarchiveURL.appending(path: "dSYMs")
 let frameworksDirectoryURL = xcarchiveURL.appending(path: "Products/Library/Frameworks")
+
+// MARK: - Convenience Methods
 
 func infoPlistContent(for frameworkName: String) -> String {
     return """
@@ -87,35 +97,32 @@ func sourceFile(including frameworkNames: [String]) -> String {
     return fileContent
 }
 
-var frameworkNames = [String]()
+// MARK: -
 
 let dSYMPaths = try! fileManager.contentsOfDirectory(atPath: dSYMsDirectoryURL.path())
 let frameworkPaths = try! fileManager.contentsOfDirectory(atPath: frameworksDirectoryURL.path())
 
-for dSYMPath in dSYMPaths {
-    guard let dSYMname = dSYMPath.split(separator: "/").last else { continue }
+// Get all framework names based on the dSYMPaths - making sure the .framework file exists
+let frameworkNames: [String] = dSYMPaths.compactMap { dSYMPath in
+    guard let dSYMname = dSYMPath.split(separator: "/").last else { return nil }
     let frameworkForDsym = String(dSYMname).replacingOccurrences(of: ".dSYM", with: "")
-    guard let frameworkPath = frameworkPaths.first(where: { $0.range(of: frameworkForDsym) != nil }) else { continue }
-    
-    let frameworkName = frameworkForDsym.replacingOccurrences(of: ".framework", with: "")
-    print("framework name:", frameworkName)
-    print("dSYM path:", currentDirectoryURL.appending(path: "\(dSYMPath)").absoluteString)
-    print("framework path:", currentDirectoryURL.appending(path: "\(frameworkPath)").absoluteString)
-    frameworkNames += [frameworkName]
+    guard frameworkPaths.first(where: { $0.range(of: frameworkForDsym) != nil }) != nil else { return nil }
+    return frameworkForDsym.replacingOccurrences(of: ".framework", with: "")
 }
 
+// Clearing and setting up the directory structure
 try? fileManager.removeItem(at: outputProjectURL)
 try! fileManager.createDirectory(at: outputProjectURL, withIntermediateDirectories: true)
 
+// Copying and setting up `.xcframework`s
 for frameworkName in frameworkNames {
     let xcframeworTargetURL = outputProjectURL.appending(path: "\(frameworkName).xcframework")
     let infoPlistTargetURL = xcframeworTargetURL.appending(path: "Info.plist")
     let libraryTargetURL = xcframeworTargetURL.appending(path: "ios-arm64_x86_64-simulator")
     let dSYMsTargetDirectoryURL = libraryTargetURL.appending(path: "dSYMs")
     
+    // Setting up framework specific folders tructure
     try! fileManager.createDirectory(at: dSYMsTargetDirectoryURL, withIntermediateDirectories: true)
-    
-    print(frameworkPaths)
     
     let dotFrameworkSourceURL = frameworksDirectoryURL.appending(path: frameworkPaths.first(where: { $0.range(of: "\(frameworkName).framework") != nil })!)
     let dotFrameworkTargetURL = libraryTargetURL.appending(path: "\(frameworkName).framework")
@@ -124,18 +131,35 @@ for frameworkName in frameworkNames {
     let dSYMtargetURL = dSYMsTargetDirectoryURL.appending(path: "\(frameworkName).framework.dSYM")
     
     print("➡️  Creating \(infoPlistTargetURL.path())")
-    fileManager.createFile(atPath: infoPlistTargetURL.path(), contents: infoPlistContent(for: frameworkName).data(using: .utf8))
+    fileManager.createFile(
+        atPath: infoPlistTargetURL.path(),
+        contents: infoPlistContent(for: frameworkName).data(using: .utf8)
+    )
     
     print("➡️  Copying dSYM: \n\(dSYMsourceURL.absoluteString) to \n\(dSYMtargetURL.absoluteString)")
-    try! fileManager.copyItem(at: dSYMsourceURL, to: dSYMtargetURL)
+    try! fileManager.copyItem(
+        at: dSYMsourceURL,
+        to: dSYMtargetURL
+    )
     print("➡️  Copying .framework: \n\(dotFrameworkSourceURL.absoluteString) to \n\(dotFrameworkTargetURL.absoluteString)")
-    try! fileManager.copyItem(at: dotFrameworkSourceURL, to: dotFrameworkTargetURL)
+    try! fileManager.copyItem(
+        at: dotFrameworkSourceURL,
+        to: dotFrameworkTargetURL
+    )
 }
 
 let packageFileTargetURL = outputProjectURL.appending(path: "Package.swift")
-fileManager.createFile(atPath: packageFileTargetURL.path(), contents: packageFileContent(for: frameworkNames, scheme: outputProjectURL.lastPathComponent ?? "").data(using: .utf8))
+fileManager.createFile(atPath: packageFileTargetURL.path(), contents: packageFileContent(for: frameworkNames, scheme: outputProjectURL.lastPathComponent).data(using: .utf8))
 
 let sourceDirectoryURL = outputProjectURL.appending(path: "Sources").appending(path: "Target")
-try! fileManager.createDirectory(at: sourceDirectoryURL, withIntermediateDirectories: true)
+try! fileManager.createDirectory(
+    at: sourceDirectoryURL,
+    withIntermediateDirectories: true
+)
+
 let sourceFileURL = sourceDirectoryURL.appending(path: "File.swift")
-fileManager.createFile(atPath: sourceFileURL.path(), contents: sourceFile(including: frameworkNames).data(using: .utf8))
+print("➡️  Creating \(sourceFileURL.path())")
+fileManager.createFile(
+    atPath: sourceFileURL.path(),
+    contents: sourceFile(including: frameworkNames).data(using: .utf8)
+)
