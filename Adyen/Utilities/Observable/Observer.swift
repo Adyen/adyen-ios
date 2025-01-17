71e7d1bbe3535008b52d1b92924fed3f39bfe63c
Alex Guretzki <goergisn@me.com>
2025-01-17 15:37:59 +0100

    switching to Void? for AssociatedKeys (#1950)
    # Summary
    - Switching to `Void?` for `AssociatedKey`s as described
    [here](https://github.com/atrick/swift-evolution/blob/diagnose-implicit-raw-bitwise/proposals/nnnn-implicit-raw-bitwise-conversion.md#workarounds-for-common-cases)
    - Removing superfluous `swiftlint:disable`
    
    # Ticket
    
    <ticket>
    COIOS-000
    </ticket>
    
    Co-authored-by: Alex Guretzki <alexander.guretzki@adyen.com>

--- Adyen/Utilities/Observable/Observer.swift
+++ Adyen/Utilities/Observable/Observer.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2021 Adyen N.V.
+// Copyright (c) 2019 Adyen N.V.
 //
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
@@ -161,6 +161,6 @@ public extension Observer {
 private enum AssociatedKeys {
     
     /// The observation manager associated with the object.
-    public static var observationManager = "observationManager"
+    public static var observationManager: Void?
     
 }
