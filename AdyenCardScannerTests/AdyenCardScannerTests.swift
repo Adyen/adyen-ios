4566f4376eca82930dba102587c6a78fdd1bd726
Naufal Aros <naufal.aros@adyen.com>
2025-02-03 11:25:20 +0100

    Create AdyenCardScanner module

---
+++ AdyenCardScannerTests/AdyenCardScannerTests.swift
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Testing
+@testable import AdyenCardScanner
+
+struct AdyenCardScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
