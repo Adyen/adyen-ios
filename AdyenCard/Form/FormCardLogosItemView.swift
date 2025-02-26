a9ffebff2aa804e7c8adce30defd0251701eb065
Naufal Aros <naufal.aros@adyen.com>
2025-02-26 11:22:39 +0100

    Update card logos layout

--- AdyenCard/Form/FormCardLogosItemView.swift
+++ AdyenCard/Form/FormCardLogosItemView.swift
@@ -31,7 +31,7 @@ internal final class FormCardLogosItemView: FormItemView<FormCardLogosItem>, UIC
     internal required init(item: FormCardLogosItem) {
         super.init(item: item)
         addSubview(collectionView)
-        collectionView.adyen.anchor(inside: self, with: UIEdgeInsets(top: 4, left: 16, bottom: 8, right: 16))
+        collectionView.adyen.anchor(inside: self.layoutMarginsGuide)
         collectionView.register(CardLogoCell.self, forCellWithReuseIdentifier: CardLogoCell.reuseIdentifier)
         collectionView.dataSource = self
     }
