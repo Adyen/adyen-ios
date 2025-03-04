f366064a13b37463b3ef510f63642b4cae33f9e5
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2025-03-04 10:27:17 +0100

    Create new accessory view for card scanner button

--- AdyenCard/Form/FormCardNumberItemView.swift
+++ AdyenCard/Form/FormCardNumberItemView.swift
@@ -19,6 +19,7 @@ internal final class FormCardNumberItemView: FormTextItemView<FormCardNumberItem
     internal required init(item: FormCardNumberItem) {
         super.init(item: item)
         accessory = .customView(detectedBrandsView)
+        textField.inputAccessoryView = makeCardScanAccessoryView(#selector(openCardScanner))
         textField.textContentType = .creditCardNumber
         textField.returnKeyType = .default
         textField.allowsEditingActions = false
@@ -95,4 +96,7 @@ internal final class FormCardNumberItemView: FormTextItemView<FormCardNumberItem
         cardTypeLogosView.backgroundColor = item.style.backgroundColor
         return cardTypeLogosView
     }()
+    
+    @objc private func openCardScanner() {
+    }
 }
