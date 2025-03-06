cc890969240f6eb0621471dfe05600125a120da4
Naufal Aros <naufal.aros@adyen.com>
2025-03-06 14:50:30 +0100

    Add CardScannerViewModelMock

---
+++ AdyenCardScannerTests/CardScannerViewControllerTests.swift
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Testing
+@testable import AdyenCardScanner
+
+struct CardScannerViewControllerTests {
+
+    var viewModel: CardScannerViewModel!
+    var sut: CardScannerViewController!
+
+    @Test
+    func exampleTests() async throws {
+        // Given
+
+        sut = CardScannerViewController(viewModel: <#T##any CardScannerViewModelProtocol#>)
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
