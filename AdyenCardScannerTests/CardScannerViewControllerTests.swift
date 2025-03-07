a9e23751bdf55f4e48db0947af1bc698d13f8cd1
Naufal Aros <naufal.aros@adyen.com>
2025-03-07 15:31:03 +0100

    Add CardScannerViewControllerTests

--- AdyenCardScannerTests/CardScannerViewControllerTests.swift
+++ AdyenCardScannerTests/CardScannerViewControllerTests.swift
@@ -4,20 +4,84 @@
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
 
-import Testing
+import XCTest
 @testable import AdyenCardScanner
 
-struct CardScannerViewControllerTests {
+final class CardScannerViewControllerTests: XCTestCase {
 
-    var viewModel: CardScannerViewModel!
+    var viewModel: CardScannerViewModelMock!
     var sut: CardScannerViewController!
 
-    @Test
-    func exampleTests() async throws {
-        // Given
-
-        sut = CardScannerViewController(viewModel: <#T##any CardScannerViewModelProtocol#>)
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    override func tearDownWithError() throws {
+        viewModel = nil
+        sut = nil
+        try super.tearDownWithError()
+    }
+
+    func testViewDidLoadShouldCallViewModelConfigureSession() throws {
+        // Given
+        viewModel = CardScannerViewModelMock()
+        sut = CardScannerViewController(viewModel: viewModel)
+
+        // When
+        sut.loadViewIfNeeded()
+
+        // Then
+        XCTAssertEqual(viewModel.configureSessionCallsCount, 1)
+    }
+
+    func testViewWillAppearShouldStartCaptureSession() throws {
+        // Given
+        viewModel = CardScannerViewModelMock()
+        sut = CardScannerViewController(viewModel: viewModel)
+
+        // When
+        sut.viewWillAppear(true)
+
+        // Then
+        XCTAssertEqual(viewModel.startCaptureSessionCallsCount, 1)
+    }
+
+    func testViewWillAppearShouldStopCaptureSession() throws {
+        // Given
+        viewModel = CardScannerViewModelMock()
+        sut = CardScannerViewController(viewModel: viewModel)
+
+        // When
+        sut.viewWillDisappear(true)
+
+        // Then
+        XCTAssertEqual(viewModel.stopCaptureSessionCallsCount, 1)
+    }
+
+    func testViewDidLayoutSubviewsShouldUpdateVideoOrientation() throws {
+        // Given
+        viewModel = CardScannerViewModelMock()
+        sut = CardScannerViewController(viewModel: viewModel)
+
+        // When
+        sut.viewDidLayoutSubviews()
+
+        // Then
+        XCTAssertEqual(viewModel.updateVideoOrientationCallsCount, 1)
+    }
+
+    func testVideoPreviewLayetIsSetOnInit() throws {
+        // Given
+        let expectedVideoPreviewLayer = CALayer()
+        viewModel = CardScannerViewModelMock()
+        viewModel.videoPreviewLayer = expectedVideoPreviewLayer
+        sut = CardScannerViewController(viewModel: viewModel)
+
+        // When
+        sut.loadViewIfNeeded()
+
+        // Then
+        let layer = try XCTUnwrap(sut.view.layer.sublayers?.first)
+        XCTAssertTrue(layer === expectedVideoPreviewLayer)
+    }
 }
