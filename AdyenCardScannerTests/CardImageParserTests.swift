ee2019c30beebec203f71b45957e85fc2a909884
Naufal Aros <naufal.aros@adyen.com>
2025-03-21 14:23:45 +0100

    Add third card image for tests

--- AdyenCardScannerTests/CardImageParserTests.swift
+++ AdyenCardScannerTests/CardImageParserTests.swift
@@ -59,6 +59,27 @@ final class CardImageParserTests: XCTestCase {
         waitForExpectations(timeout: 10.0)
     }
 
+    func testParseImageWithThirdImage() throws {
+        // Given
+        let expirationDateFormatter = ExpirationDateFormatter()
+        sut = CardImageParser(expirationDateFormatter: expirationDateFormatter)
+
+        let testCreditCard = try XCTUnwrap(testCreditCardThree)
+        let expectation = expectation(description: "Image should be parsed")
+
+        // When
+        sut.parse(image: testCreditCard.image) { receivedCreditCard in
+            expectation.fulfill()
+
+            let expectedCreditCard = testCreditCard.creditCard
+            XCTAssertEqual(expectedCreditCard.number, receivedCreditCard.number)
+            XCTAssertEqual(expectedCreditCard.expirationDate, receivedCreditCard.expirationDate)
+        }
+
+        // Then
+        waitForExpectations(timeout: 10.0)
+    }
+
     // MARK: - Private
 
     private struct TestCreditCard {
@@ -103,6 +124,26 @@ final class CardImageParserTests: XCTestCase {
             creditCard: creditCard
         )
     }
+
+    private var testCreditCardThree: TestCreditCard? {
+        let image = UIImage(
+            named: "test-card-number-3",
+            in: Bundle(for: type(of: self)),
+            compatibleWith: nil
+        )
+        guard let cgImage = image?.cgImage else { return nil }
+        let originalImage = CIImage(cgImage: cgImage)
+
+        let creditCard = CreditCard(
+            number: "4000123456789010",
+            expirationDate: dateFrom("12/20")
+        )
+        return TestCreditCard(
+            image: originalImage,
+            creditCard: creditCard
+        )
+    }
+
 
     private func dateFrom(_ string: String) -> Date? {
         let dateFormatter = ExpirationDateFormatter()
