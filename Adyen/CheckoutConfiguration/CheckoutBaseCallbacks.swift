7e96c93ec4ff34576f264ef5e99b9a26d5431db2
erenbesel <erenbesel@gmail.com>
2025-04-29 14:48:10 +0200

    move callbacks to own file

---
+++ Adyen/CheckoutConfiguration/CheckoutBaseCallbacks.swift
@@ -0,0 +1,29 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Foundation
+
+// TODO: Finalize all the parameters of the callbacks
+// Move Action to core module?
+// Add Resultcode enum
+public typealias PaymentsResponseHandler = (_ resultCode: String, _ action: String) -> Void
+public typealias SubmitHandler = (_ data: PaymentComponentData, _ handler: PaymentsResponseHandler?) -> Void
+public typealias AdditionalDetailsHandler = (_ data: ActionComponentData, _ handler: PaymentsResponseHandler?) -> Void
+// TODO: Have a checkout error object?
+public typealias CheckoutErrorHandler = (_ error: Error) -> Void
+public typealias CheckoutSuccessHandler = (_ resultCode: String) -> Void
+
+/// Basic callbacks for all components.
+package protocol CheckoutBaseCallbacks {
+    
+    var onSubmit: SubmitHandler? { get set }
+    
+    var onAdditionalDetails: AdditionalDetailsHandler? { get set }
+    
+    var onError: CheckoutErrorHandler? { get set }
+    
+    var onComplete: CheckoutSuccessHandler? { get set }
+}
