0b181abf970be929ae9bc8d45b5f809b6e85de9a
Robert D'Almeida <robert.dalmeida@adyen.com>
2025-05-15 23:49:14 +0200

    update and build

--- Scripts/test-carthage-integration.sh
+++ Scripts/test-carthage-integration.sh
@@ -55,7 +55,8 @@ then
   echo "github \"adyen/adyen-authentication-ios\" == 3.1.0" >> Cartfile
   echo_header "Swift Version:" | tee -a  "${LOGFILE}"
   xcrun --sdk iphoneos swiftc --version | tee -a  "${LOGFILE}"
-  carthage update --platform iOS --use-xcframeworks --no-skip-current 2>&1 | tee -a  "${LOGFILE}"
+  carthage update --platform iOS --use-xcframeworks 2>&1 | tee -a  "${LOGFILE}"
+  carthage build --platform iOS --use-xcframeworks --no-skip-current --configuration Debug 2>&1 | tee -a "${LOGFILE}"
   
 else
   cd $PROJECT_NAME
