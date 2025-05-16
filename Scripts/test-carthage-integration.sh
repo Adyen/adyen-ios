c70cb4955ae4d4d4dd33402a2ce89b3ae064cfb3
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2025-05-16 15:07:31 +0200

    Remove Carthage cache before fetching/compiling dependencies

--- Scripts/test-carthage-integration.sh
+++ Scripts/test-carthage-integration.sh
@@ -47,9 +47,13 @@ then
   CWD=$(pwd)
   CURRENT_COMMIT=$(git rev-parse HEAD)
 
+  rm -rf Carthage
+  rm -rf ~/Library/Caches/org.carthage.CarthageKit
+  rm -rf ~/Library/Developer/Xcode/DerivedData
+
   echo "git \"file://$CWD/../\" \"$CURRENT_COMMIT\"" > Cartfile
   echo "github \"adyen/adyen-authentication-ios\" == 3.1.0" >> Cartfile
-  carthage update --no-use-binaries --use-xcframeworks --configuration Debug
+  carthage update --use-xcframeworks --configuration Debug
 else
   cd $PROJECT_NAME
 fi
