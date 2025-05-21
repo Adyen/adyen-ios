f65dd272b3a80bb6e9eb03b395891e6604171ee5
Neelam Sharma <neelam.sharma@adyen.com>
2025-05-21 15:07:23 +0200

    Renamed DualBrandView to DualBrandAccessoryView

--- AdyenCard/Form/FormCoBadgedCardItemStyle.swift
+++ AdyenCard/Form/FormCoBadgedCardItemStyle.swift
@@ -4,7 +4,7 @@
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
 
-import Foundation
+@_spi(AdyenInternal) import Adyen
 import UIKit
 
 /// Contains the styling customization options for an item in a FormCoBadged card Item.
