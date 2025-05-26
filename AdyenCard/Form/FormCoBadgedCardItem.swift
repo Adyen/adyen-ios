4436e540fcdfa4981a9e2b1ffd0eaa45f93aaf43
Neelam Sharma <neelam.sharma@adyen.com>
2025-05-26 18:00:05 +0200

    Added comment for card name title

--- AdyenCard/Form/FormCoBadgedCardItem.swift
+++ AdyenCard/Form/FormCoBadgedCardItem.swift
@@ -79,6 +79,7 @@ internal final class FormCoBadgedCardItem: FormItem {
 
             let isSelected = brand.type.rawValue == defaultSelectedBrand.type.rawValue ? true : false
 
+            /// Title should be 'localeBrand' and if it is nil then use 'brand' property from binLookup
             let selectableItem = SelectableFormItem(
                 title: brand.localeBrand ?? brand.type.rawValue,
                 imageUrl: brandLogoURL,
