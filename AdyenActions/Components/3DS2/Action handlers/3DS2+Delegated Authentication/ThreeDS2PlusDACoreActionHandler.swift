500cfc2a10507dbe46a3106c133376c16c941c8b
Robert D'Almeida <robert.dalmeida@adyen.com>
2025-06-06 12:49:46 +0200

    DSP-2591 - on cancell of a challenge don't prompt for passkey enrollment

--- AdyenActions/Components/3DS2/Action handlers/3DS2+Delegated Authentication/ThreeDS2PlusDACoreActionHandler.swift
+++ AdyenActions/Components/3DS2/Action handlers/3DS2+Delegated Authentication/ThreeDS2PlusDACoreActionHandler.swift
@@ -440,6 +440,10 @@ internal typealias VoidHandler = () -> Void
             }
         }
         
+        internal override func userCancelledChallenge() {
+            self.delegatedAuthenticationState.attemptRegistration = false
+        }
+        
         private enum RegistrationFlowError: Error {
             case registrationServiceError(underlyingError: Error)
             case userOptedOutOfRegistration
