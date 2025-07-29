3c5f32cdcf29a7ff2f4cc5b7ecd358f2140d0a7a
Naufal Aros <naufal.aros@adyen.com>
2025-07-29 17:18:48 +0200

    Update

--- Scripts/publish-demo-app.sh
+++ Scripts/publish-demo-app.sh
@@ -2,43 +2,59 @@
 
 set -euo pipefail
 
+BUILD_PATH=Build-Temp
 APPLE_ID_USERNAME="$1"
 APPLE_APP_SPECIFIC_PASSWORD="$2"
-BUILD_NUMBER="$3"
+XCODE_AUTHENTICATION_KEY_PATH="$3"
+BUILD_NUMBER="$4"
 
 echo "🔧 Building Demo app for distribution..."
 
-xcodebuild clean -scheme AdyenUIHost -project Adyen.xcodeproj
+xcodebuild clean \
+    -project Adyen.xcodeproj \
+    -scheme AdyenUIHost \
+    -destination="generic/platform=iOS" \
+    -sdk iphoneos \
+    -configuration Release \
+    -skipPackagePluginValidation
+
+mkdir -p $BUILD_PATH
 
 xcodebuild archive \
-  -project Adyen.xcodeproj \
-  -scheme AdyenUIHost \
-  -configuration Release \
-  -archivePath ./Build-Temp/AdyenUIHost.xcarchive \
-  -destination "generic/platform=iOS" \
-  -allowProvisioningUpdates \
-  -allowProvisioningDeviceRegistration \
-  -derivedDataPath ./Build-Temp/DerivedData \
-  -quiet \
-  OTHER_CODE_SIGN_FLAGS="--keychain $RUNNER_TEMP/app-signing.keychain-db" \
-  CODE_SIGN_STYLE=Manual \
-  DEVELOPMENT_TEAM=$APPLE_DEVELOPMENT_TEAM_ID \
-  PRODUCT_BUNDLE_IDENTIFIER=$APP_BUNDLE_ID \
-  BUILD_NUMBER=$BUILD_NUMBER
+    -project Adyen.xcodeproj \
+    -scheme AdyenUIHost \
+    -destination="generic/platform=iOS" \
+    -sdk iphoneos \
+    -configuration Release \
+    -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
+    -allowProvisioningUpdates \
+    -allowProvisioningDeviceRegistration \
+    -derivedDataPath $BUILD_PATH/DerivedData
+    -skipPackagePluginValidation \
+    -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
+    -authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
+    -authenticationKeyPath $XCODE_AUTHENTICATION_KEY_PATH \
+    -quiet \
+    CODE_SIGN_STYLE=Manual \
+    BUILD_NUMBER=$BUILD_NUMBER
 
 echo "📦 Exporting IPA..."
 
 xcodebuild -exportArchive \
-  -archivePath ./Build-Temp/AdyenUIHost.xcarchive \
-  -exportPath ./Build-Temp \
-  -exportOptionsPlist ./exportOptions.plist \
-  -quiet
+    -archivePath $BUILD_PATH/AdyenUIHost.xcarchive \
+    -exportOptionsPlist exportOptions.plist \
+    -exportPath $BUILD_PATH \
+    -allowProvisioningUpdates \
+    -skipPackagePluginValidation \
+    -authenticationKeyID $XCODE_AUTHENTICATION_KEY_ID \
+    -authenticationKeyIssuerID $XCODE_AUTHENTICATION_KEY_ISSUER_ID \
+    -authenticationKeyPath $3
 
 echo "☁️ Uploading to App Store Connect..."
 
 xcrun altool --upload-app \
   --type ios \
-  --file ./Build-Temp/AdyenUIHost.ipa \
+  --file $BUILD_PATH/AdyenUIHost.ipa \
   --username "$APPLE_ID_USERNAME" \
   --password "$APPLE_APP_SPECIFIC_PASSWORD" \
   --output-format xml
