6cd49d52634724c5ec45e7d57cbd9f471b67c43d
Neelam Sharma <neelam.sharma@adyen.com>
2025-08-21 16:02:45 +0200

    Added tests

--- Tests/UnitTests/Core/AdyenContextTests.swift
+++ Tests/UnitTests/Core/AdyenContextTests.swift
@@ -89,8 +89,7 @@ class AdyenContextTests: XCTestCase {
     }
     
     func testOnlyAnalyticsProviderShouldBeCreated() {
-        var config = AnalyticsConfiguration()
-        config.isEnabled = false
+        var config = AnalyticsConfiguration(isEnabled: false)
         
         let context = AdyenContext(
             apiContext: Dummy.apiContext,
