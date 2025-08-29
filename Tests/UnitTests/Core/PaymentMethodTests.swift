81d0e8294c5217933fe6ce6a9866b34f7f0c2d3b
Naufal Aros <naufal.aros@adyen.com>
2025-08-29 12:54:06 +0200

    Reverted changed done by linter

--- Tests/UnitTests/Core/PaymentMethodTests.swift
+++ Tests/UnitTests/Core/PaymentMethodTests.swift
@@ -845,7 +845,7 @@ class PaymentMethodTests: XCTestCase {
         testCoding(paymentMethod)
     }
     
-    func expectedStoredCardPaymentMethodDisplayInfo(method: StoredCardPaymentMethod, localizationParameters: LocalizationParameters?) -> DisplayInformation {
+    public func expectedStoredCardPaymentMethodDisplayInfo(method: StoredCardPaymentMethod, localizationParameters: LocalizationParameters?) -> DisplayInformation {
         let expireDate = method.expiryMonth + "/" + method.expiryYear.suffix(2)
         if let customDisplayInformation = method.merchantProvidedDisplayInformation {
             return DisplayInformation(
@@ -1038,7 +1038,7 @@ class PaymentMethodTests: XCTestCase {
         testCoding(paymentMethod)
     }
     
-    func expectedBancontactCardDisplayInfo(
+    public func expectedBancontactCardDisplayInfo(
         method: StoredBCMCPaymentMethod,
         localizationParameters: LocalizationParameters?
     ) -> DisplayInformation {
