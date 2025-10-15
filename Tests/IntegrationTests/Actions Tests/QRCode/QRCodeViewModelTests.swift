831beb8c5c7f3777d9b8956ce4abfc7045a36571
Naufal Aros <naufal.aros@adyen.com>
2025-10-15 14:44:01 +0200

    Update QRCodeViewModelTests

--- Tests/IntegrationTests/Actions Tests/QRCode/QRCodeViewModelTests.swift
+++ Tests/IntegrationTests/Actions Tests/QRCode/QRCodeViewModelTests.swift
@@ -15,7 +15,7 @@ class QRCodeViewModelTests: XCTestCase {
     
     func test_flowType_forPix_returnsCopyCode() {
         // Given
-        let (sut, _, _) = makeSUT(paymentMethodType: .pix)
+        let (sut, _) = makeSUT(paymentMethodType: .pix)
         
         // When
         XCTAssertEqual(sut.flowType, .copyCode)
@@ -23,7 +23,7 @@ class QRCodeViewModelTests: XCTestCase {
     
     func test_flowType_forPromptPay_returnsSaveCodeAsImage() {
         // Given
-        let (sut, _, _) = makeSUT(paymentMethodType: .promptPay)
+        let (sut, _) = makeSUT(paymentMethodType: .promptPay)
         
         // When
         XCTAssertEqual(sut.flowType, .saveCodeAsImage)
@@ -36,7 +36,7 @@ class QRCodeViewModelTests: XCTestCase {
         let qrCodeData = "ABC123"
         
         // When
-        let (sut, _, _) = makeSUT(qrCodeData: qrCodeData)
+        let (sut, _) = makeSUT(qrCodeData: qrCodeData)
         
         // Then
         XCTAssertEqual(sut.qrCodeData, qrCodeData)
@@ -47,7 +47,7 @@ class QRCodeViewModelTests: XCTestCase {
     func test_loadLogoImage_callsImageLoaderWithURL() {
         // Given
         let imageLoader = ImageLoaderMock()
-        let (sut, _, loader) = makeSUT(imageLoader: imageLoader)
+        let (sut, loader) = makeSUT(imageLoader: imageLoader)
         
         // When
         sut.loadLogoImage { _ in }
@@ -60,10 +60,11 @@ class QRCodeViewModelTests: XCTestCase {
     
     func test_performAction_copyCode_callsStartCopyAnimationAndOnCopyCode() throws {
         // Given
+        let expectedView = UIView()
         let copyCodeExpectation = expectation(description: "copyCode was not called")
         var receivedCode: String?
         
-        let (sut, view, _) = makeSUT(
+        let (sut, _) = makeSUT(
             paymentMethodType: .pix,
             onCopyCode: ({ code in
                 copyCodeExpectation.fulfill()
@@ -72,11 +73,10 @@ class QRCodeViewModelTests: XCTestCase {
         )
         
         // When
-        sut.performAction(qrCodeImage: nil)
+        sut.performAction(qrCodeImage: nil, from: expectedView)
         
         // Then
         wait(for: [copyCodeExpectation], timeout: 0.1)
-        XCTAssertEqual(view.startCopyAnimationCallsCount, 1)
         XCTAssertEqual(sut.qrCodeData, receivedCode)
     }
     
@@ -84,13 +84,14 @@ class QRCodeViewModelTests: XCTestCase {
     
     func test_performAction_saveCodeAsImage_callsOnSaveQRCode() {
         // Given
+        let expectedView = UIView()
         let expectedImage = UIImage()
         let onSaveQRCodeExpectation = expectation(description: "onSaveQRCode was not called")
         
         var receivedImage: UIImage?
         var receivedSourceView: UIView?
         
-        let (sut, view, _) = makeSUT(
+        let (sut, _) = makeSUT(
             paymentMethodType: .promptPay,
             onSaveQRCode: ({ qrCodeImage, sourceView in
                 onSaveQRCodeExpectation.fulfill()
@@ -101,12 +102,12 @@ class QRCodeViewModelTests: XCTestCase {
         )
         
         // When
-        sut.performAction(qrCodeImage: expectedImage)
+        sut.performAction(qrCodeImage: expectedImage, from: expectedView)
         
         // Then
         wait(for: [onSaveQRCodeExpectation], timeout: 0.1)
         XCTAssertEqual(expectedImage, receivedImage)
-        XCTAssertEqual(view.rootView, receivedSourceView)
+        XCTAssertEqual(expectedView, receivedSourceView)
     }
     
     func test_performAction_saveCodeAsImage_noImage_doesNotCallOnSaveQRCode() {
@@ -114,7 +115,7 @@ class QRCodeViewModelTests: XCTestCase {
         let onSaveQRCodeExpectation = expectation(description: "onSaveQRCode was called")
         onSaveQRCodeExpectation.isInverted = true
         
-        let (sut, _, _) = makeSUT(
+        let (sut, _) = makeSUT(
             paymentMethodType: .promptPay,
             onSaveQRCode: ({ _, _ in
                 onSaveQRCodeExpectation.fulfill()
@@ -122,7 +123,7 @@ class QRCodeViewModelTests: XCTestCase {
         )
         
         // When
-        sut.performAction(qrCodeImage: nil)
+        sut.performAction(qrCodeImage: nil, from: UIView())
         
         // Then
         wait(for: [onSaveQRCodeExpectation], timeout: 1.0)
@@ -132,7 +133,7 @@ class QRCodeViewModelTests: XCTestCase {
     
     func test_actionButtonTitle_and_onCopyButtonTitle_forCopyCode() {
         // Given
-        let (sut, _, _) = makeSUT(paymentMethodType: .pix)
+        let (sut, _) = makeSUT(paymentMethodType: .pix)
         
         // Then
         XCTAssertEqual(sut.actionButtonTitle, localizedString(.pixCodeCopyLabel, nil))
@@ -141,7 +142,7 @@ class QRCodeViewModelTests: XCTestCase {
     
     func test_actionButtonTitle_and_onCopyButtonTitle_forSaveCodeAsImage() {
         // Given
-        let (sut, _, _) = makeSUT(paymentMethodType: .promptPay)
+        let (sut, _) = makeSUT(paymentMethodType: .promptPay)
         
         // Then
         XCTAssertEqual(sut.actionButtonTitle, localizedString(.voucherSaveImage, nil))
@@ -157,7 +158,7 @@ class QRCodeViewModelTests: XCTestCase {
         imageLoader: ImageLoaderMock = ImageLoaderMock(),
         onSaveQRCode: @escaping (_ image: UIImage?, _ sourceView: UIView) -> Void = { _, _ in },
         onCopyCode: @escaping (_ code: String) -> Void = { _ in }
-    ) -> (sut: QRCodeViewModel, view: QRCodeViewMock, imageLoader: ImageLoaderMock) {
+    ) -> (sut: QRCodeViewModel, imageLoader: ImageLoaderMock) {
         
         let action = QRCodeAction(
             paymentMethodType: paymentMethodType,
@@ -165,7 +166,6 @@ class QRCodeViewModelTests: XCTestCase {
             paymentData: paymentData
         )
         let expiration = AdyenObservable<String?>(nil)
-        let view = QRCodeViewMock()
         
         let sut = QRCodeViewModel(
             action: action,
@@ -179,8 +179,7 @@ class QRCodeViewModelTests: XCTestCase {
             onSaveQRCode: onSaveQRCode,
             onCopyCode: onCopyCode
         )
-        sut.view = view
         
-        return (sut, view, imageLoader)
+        return (sut, imageLoader)
     }
 }
