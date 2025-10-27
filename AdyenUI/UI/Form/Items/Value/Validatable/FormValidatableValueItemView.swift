709fd1ea6225c504fe7c34222aa4df88f75e78cc
Neelam Sharma <neelam.sharma@adyen.com>
2025-10-27 14:19:52 +0100

    Added todo:

--- AdyenUI/UI/Form/Items/Value/Validatable/FormValidatableValueItemView.swift
+++ AdyenUI/UI/Form/Items/Value/Validatable/FormValidatableValueItemView.swift
@@ -99,6 +99,7 @@ open class FormValidatableValueItemView<ValueType, ItemType: FormValidatableValu
     
     internal func resetValidationStatus() {
         hideAlertLabel(true, animated: false)
+        // TODO: Replace AdyenTheme with config's object theme style
         titleLabel.textColor = AdyenTheme().currentColorScheme.primary
         accessibilityLabelView?.accessibilityLabel = item.title
     }
