e6bb2f377856219c072f2fda68efa6339c91290a
Neelam Sharma <neelam.sharma@adyen.com>
2025-10-31 09:01:29 +0100

    Apply new toggle styling in card component

--- AdyenUI/Styles/AdyenToggleStyle.swift
+++ AdyenUI/Styles/AdyenToggleStyle.swift
@@ -10,25 +10,32 @@ import UIKit
 package struct AdyenToggleStyle {
     
     /// The title style.
-    var title = AdyenLabelStyle()
+    package var title: AdyenLabelStyle
     
     /// The color of `onTintColor` of switch.
     package var tintColor: UIColor?
 
     // The background color of the switch.
-    package var backgroundColor: UIColor = .clear
+    package var backgroundColor: UIColor
 
-    var cornerRadius: CornerRounding = .fixed(AdyenUIConstants.defaultCornerRadius)
+    package var cornerRadius: CornerRounding
     
     /// Initializes the form switch item style.
     ///
     /// - Parameter title: The title style.
     package init(title: AdyenLabelStyle) {
+        self.init()
         self.title = title
     }
     
     /// Initializes the form switch item style with the default style.
-    package init() {}
+    package init() {
+        title = AdyenLabelStyle()
+        tintColor = AdyenColorScheme.default.primary
+        backgroundColor = AdyenColorScheme.default.container
+        cornerRadius = .fixed(AdyenUIConstants.defaultCornerRadius)
+    }
+    
 }
 
 // This extension adds the method chaining to the AdyenToggleStyle struct.
@@ -37,7 +44,7 @@ extension AdyenToggleStyle {
     /// Returns a new AdyenToggleStyle with the specified title.
     /// - Parameter title: The title to set.
     /// - Returns: A new `AdyenToggleStyle` instance.
-    func title(_ title: AdyenLabelStyle) -> AdyenToggleStyle {
+    package func title(_ title: AdyenLabelStyle) -> AdyenToggleStyle {
         var newStyle = self
         newStyle.title = title
         return newStyle
@@ -46,7 +53,7 @@ extension AdyenToggleStyle {
     /// Returns a new AdyenToggleStyle with the specified tintColor.
     /// - Parameter tintColor: The tintColor to set.
     /// - Returns: A new `AdyenToggleStyle` instance.
-    func tintColor(_ tintColor: UIColor) -> AdyenToggleStyle {
+    package func tintColor(_ tintColor: UIColor) -> AdyenToggleStyle {
         var newStyle = self
         newStyle.tintColor = tintColor
         return newStyle
@@ -55,7 +62,7 @@ extension AdyenToggleStyle {
     /// Returns a new AdyenToggleStyle with the specified backgroundColor.
     /// - Parameter backgroundColor: The backgroundColor to set.
     /// - Returns: A new `AdyenToggleStyle` instance.
-    func backgroundColor(_ backgroundColor: UIColor) -> AdyenToggleStyle {
+    package func backgroundColor(_ backgroundColor: UIColor) -> AdyenToggleStyle {
         var newStyle = self
         newStyle.backgroundColor = backgroundColor
         return newStyle
@@ -64,7 +71,7 @@ extension AdyenToggleStyle {
     /// Returns a new AdyenToggleStyle with the specified cornerRadius.
     /// - Parameter cornerRadius: The cornerRadius to set.
     /// - Returns: A new `AdyenToggleStyle` instance.
-    func cornerRadius(_ cornerRadius: CornerRounding) -> AdyenToggleStyle {
+    package func cornerRadius(_ cornerRadius: CornerRounding) -> AdyenToggleStyle {
         var newStyle = self
         newStyle.cornerRadius = cornerRadius
         return newStyle
--- AdyenUI/UI/Form/Items/Toggle/FormToggleItemView.swift
+++ AdyenUI/UI/Form/Items/Toggle/FormToggleItemView.swift
@@ -11,10 +11,13 @@ import UIKit
 @_spi(AdyenInternal)
 public final class FormToggleItemView: FormItemView<FormToggleItem> {
 
+    // TODO: TO be passed as a dependency by FormViewController.ItemManager
+    package let style: AdyenToggleStyle = .init()
+
     // MARK: - UI elements
     
     private lazy var label: UILabel = {
-        let label = UILabel(style: item.style.title)
+        let label = UILabel()
         label.text = item.title
         label.numberOfLines = 0
         label.accessibilityIdentifier = item.identifier.map {
@@ -28,7 +31,11 @@ public final class FormToggleItemView: FormItemView<FormToggleItem> {
         stackView.axis = .horizontal
         stackView.alignment = .center
         stackView.distribution = .fill
-        stackView.spacing = 8.0
+        stackView.spacing = AdyenUIConstants.stackViewSpacing
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = AdyenUIConstants.contentInsets
+        stackView.preservesSuperviewLayoutMargins = true
+      
         return stackView
     }()
 
@@ -36,7 +43,6 @@ public final class FormToggleItemView: FormItemView<FormToggleItem> {
         let switchControl = UISwitch()
         switchControl.translatesAutoresizingMaskIntoConstraints = false
         switchControl.isOn = item.value
-        switchControl.onTintColor = item.style.tintColor
         switchControl.isAccessibilityElement = true
         switchControl.setContentHuggingPriority(.required, for: .horizontal)
         switchControl.setContentCompressionResistancePriority(.required, for: .horizontal)
@@ -52,9 +58,9 @@ public final class FormToggleItemView: FormItemView<FormToggleItem> {
     /// - Parameter item: The item represented by the view.
     public required init(item: FormToggleItem) {
         super.init(item: item)
-        
-        backgroundColor = item.style.backgroundColor
 
+        applyAdyenStyle(style)
+        
         isAccessibilityElement = false
         accessibilityTraits = switchControl.accessibilityTraits
         accessibilityValue = switchControl.accessibilityValue
@@ -75,6 +81,31 @@ public final class FormToggleItemView: FormItemView<FormToggleItem> {
     override public func reset() {
         item.value = false
     }
+    
+    // MARK: - AdyenTheme
+
+    /// Applies all the style properties from AdyenToggleStyle to the FormToggleItemView.
+    ///
+    /// - Parameter style: The style to apply.
+    private func applyAdyenStyle(_ style: AdyenToggleStyle) {
+        
+        // View's background color
+        stackView.backgroundColor = style.backgroundColor
+        
+        // ToggleView's label 
+        label.font = style.title.font
+        
+        // SwitchControl
+        switchControl.onTintColor = style.tintColor
+        
+        switch style.cornerRadius {
+        case let .fixed(radius):
+            stackView.layer.cornerRadius = radius
+        default:
+            break
+        }
+    }
+
 }
 
 // MARK: - Private
