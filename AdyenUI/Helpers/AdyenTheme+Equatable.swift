33e3594310af0b87b9d4b106457bdce126b5c668
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2025-11-05 20:28:50 +0100

    Move Equatable back to the main target to satisfy access modifiers requirements

--- AdyenUI/Helpers/AdyenTheme+Equatable.swift
+++ AdyenUI/Helpers/AdyenTheme+Equatable.swift
@@ -4,9 +4,6 @@
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
 
-import AdyenUI
-import UIKit
-
 extension AdyenButtonStyle: Equatable {
     public static func == (lhs: AdyenButtonStyle, rhs: AdyenButtonStyle) -> Bool {
         lhs.textColor == rhs.textColor && lhs.backgroundColor == rhs.backgroundColor
@@ -25,53 +22,39 @@ extension AdyenLabelStyle: Equatable {
 
 extension AdyenButtonStyles: Equatable {
     public static func == (lhs: AdyenButtonStyles, rhs: AdyenButtonStyles) -> Bool {
-        lhs.primary == rhs.primary &&
-            lhs.secondary == rhs.secondary &&
-            lhs.tertiary == rhs.tertiary &&
-            lhs.destructive == rhs.destructive
+        lhs.primary == rhs.primary && lhs.secondary == rhs.secondary && lhs.tertiary == rhs.tertiary
+            && lhs.destructive == rhs.destructive
     }
 }
 
 extension AdyenLabelStyles: Equatable {
     public static func == (lhs: AdyenLabelStyles, rhs: AdyenLabelStyles) -> Bool {
-        lhs.title == rhs.title &&
-            lhs.subtitle == rhs.subtitle &&
-            lhs.body == rhs.body &&
-            lhs.bodyEmphasized == rhs.bodyEmphasized &&
-            lhs.subheadline == rhs.subheadline &&
-            lhs.subheadlineEmphasized == rhs.subheadlineEmphasized &&
-            lhs.footnote == rhs.footnote &&
-            lhs.footnoteEmphasized == rhs.footnoteEmphasized
+        lhs.title == rhs.title && lhs.subtitle == rhs.subtitle && lhs.body == rhs.body
+            && lhs.bodyEmphasized == rhs.bodyEmphasized && lhs.subheadline == rhs.subheadline
+            && lhs.subheadlineEmphasized == rhs.subheadlineEmphasized
+            && lhs.footnote == rhs.footnote && lhs.footnoteEmphasized == rhs.footnoteEmphasized
     }
 }
 
 extension AdyenSwitchStyle: Equatable {
     public static func == (lhs: AdyenSwitchStyle, rhs: AdyenSwitchStyle) -> Bool {
-        lhs.backgroundColor == rhs.backgroundColor &&
-            lhs.tintColor == rhs.tintColor &&
-            lhs.title == rhs.title &&
-            lhs.cornerRadius == rhs.cornerRadius
+        lhs.backgroundColor == rhs.backgroundColor && lhs.tintColor == rhs.tintColor
+            && lhs.title == rhs.title && lhs.cornerRadius == rhs.cornerRadius
     }
 }
 
 extension AdyenTextFieldStyle: Equatable {
     public static func == (lhs: AdyenTextFieldStyle, rhs: AdyenTextFieldStyle) -> Bool {
-        lhs.backgroundColor == rhs.backgroundColor &&
-            lhs.errorColor == rhs.errorColor &&
-            lhs.borderColor == rhs.borderColor &&
-            lhs.borderWidth == rhs.borderWidth &&
-            lhs.cornerRadius == rhs.cornerRadius &&
-            lhs.title == rhs.title &&
-            lhs.text == rhs.text &&
-            lhs.placeholder == rhs.placeholder
+        lhs.backgroundColor == rhs.backgroundColor && lhs.errorColor == rhs.errorColor
+            && lhs.borderColor == rhs.borderColor && lhs.borderWidth == rhs.borderWidth
+            && lhs.cornerRadius == rhs.cornerRadius && lhs.title == rhs.title
+            && lhs.text == rhs.text && lhs.placeholder == rhs.placeholder
     }
 }
 
 extension AdyenElements: Equatable {
     public static func == (lhs: AdyenElements, rhs: AdyenElements) -> Bool {
-        lhs.buttons == rhs.buttons &&
-            lhs.labels == rhs.labels &&
-            lhs.switch == rhs.switch &&
-            lhs.textField == rhs.textField
+        lhs.buttons == rhs.buttons && lhs.labels == rhs.labels && lhs.switch == rhs.switch
+            && lhs.textField == rhs.textField
     }
 }
