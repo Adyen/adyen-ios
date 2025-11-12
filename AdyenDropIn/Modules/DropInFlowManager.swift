a97c5bdc7220e4606121bfc8ed79240ebb6ef499
Naufal Aros <naufal.aros@adyen.com>
2025-11-12 17:22:09 +0100

    Define actionComponent in DFM

--- AdyenDropIn/Modules/DropInFlowManager.swift
+++ AdyenDropIn/Modules/DropInFlowManager.swift
@@ -6,14 +6,11 @@
 
 import Foundation
 import Adyen
+import AdyenActions
 
 internal protocol DropInFlowManaging {
     func submit(_ data: PaymentComponentData, from component: PaymentComponent)
     func fail(with error: Error, from component: PaymentComponent)
-    func provide(_ data: ActionComponentData, from component: ActionComponent)
-    func complete(from component: ActionComponent)
-    func fail(with error: Error, from component: ActionComponent)
-    func openExternalApplication(component: ActionComponent)
     func fail(with error: Error)
     func cancel(component: PaymentComponent)
 }
@@ -24,16 +21,35 @@ internal class DropInFlowManager: DropInFlowManaging {
 
     private weak var dropInComponent: DropInComponent?
     private weak var dropInComponentDelegate: DropInComponentDelegate?
+    private let context: AdyenContext
+    private let configuration: DropInComponent.Configuration
 
     // MARK: - Initializers
 
     internal init(
         dropInComponent: DropInComponent,
-        dropInComponentDelegate: DropInComponentDelegate
+        dropInComponentDelegate: DropInComponentDelegate,
+        context: AdyenContext,
+        configuration: DropInComponent.Configuration
     ) {
         self.dropInComponent = dropInComponent
         self.dropInComponentDelegate = dropInComponentDelegate
+        self.context = context
+        self.configuration = configuration
     }
+
+    // MARK: - Private
+
+    private lazy var actionComponent: AdyenActionComponent = {
+        let actionComponent = AdyenActionComponent(context: context)
+        actionComponent.delegate = self
+        actionComponent.presentationDelegate = self
+        actionComponent.configuration.style = configuration.style.actionComponent
+        actionComponent.configuration.localizationParameters = configuration.localizationParameters
+        actionComponent.configuration.threeDS = configuration.actionComponent.threeDS
+        actionComponent.configuration.twint = configuration.actionComponent.twint
+        return actionComponent
+    }()
 
     // MARK: - DropInFlowManaging
 
@@ -47,26 +63,6 @@ internal class DropInFlowManager: DropInFlowManaging {
         dropInComponentDelegate?.didFail(with: error, from: component, in: dropInComponent)
     }
 
-    func provide(_ data: ActionComponentData, from component: ActionComponent) {
-        guard let dropInComponent else { return }
-        dropInComponentDelegate?.didProvide(data, from: component, in: dropInComponent)
-    }
-
-    func complete(from component: ActionComponent) {
-        guard let dropInComponent else { return }
-        dropInComponentDelegate?.didComplete(from: component, in: dropInComponent)
-    }
-
-    func fail(with error: Error, from component: ActionComponent) {
-        guard let dropInComponent else { return }
-        dropInComponentDelegate?.didFail(with: error, from: component, in: dropInComponent)
-    }
-
-    func openExternalApplication(component: ActionComponent) {
-        guard let dropInComponent else { return }
-        dropInComponentDelegate?.didOpenExternalApplication(component: component, in: dropInComponent)
-    }
-
     func fail(with error: Error) {
         guard let dropInComponent else { return }
         dropInComponentDelegate?.didFail(with: error, from: dropInComponent)
@@ -77,3 +73,45 @@ internal class DropInFlowManager: DropInFlowManaging {
         dropInComponentDelegate?.didCancel(component: component, from: dropInComponent)
     }
 }
+
+// MARK: - ActionComponentDelegate
+
+extension DropInFlowManager: ActionComponentDelegate {
+
+    internal func didOpenExternalApplication(component: any ActionComponent) {
+        component.stopLoading()
+
+        guard let dropInComponent else { return }
+        dropInComponentDelegate?.didOpenExternalApplication(component: component, in: dropInComponent)
+    }
+
+    internal func didProvide(_ data: ActionComponentData, from component: any ActionComponent) {
+        guard let dropInComponent else { return }
+        dropInComponentDelegate?.didProvide(data, from: component, in: dropInComponent)
+    }
+
+    internal func didComplete(from component: any ActionComponent) {
+        guard let dropInComponent else { return }
+        dropInComponentDelegate?.didComplete(from: component, in: dropInComponent)
+    }
+
+    internal func didFail(with error: any Error, from component: any ActionComponent) {
+        guard let dropInComponent else { return }
+
+        if case ComponentError.cancelled = error {
+            // TODO: - Handle action cancellation
+        } else {
+            dropInComponentDelegate?.didFail(with: error, from: component, in: dropInComponent)
+        }
+    }
+}
+
+// MARK: - PresentationDelegate
+
+extension DropInFlowManager: PresentationDelegate {
+
+    internal func present(component: any PresentableComponent) {
+        // TODO: - Handle presentation
+    }
+}
+
