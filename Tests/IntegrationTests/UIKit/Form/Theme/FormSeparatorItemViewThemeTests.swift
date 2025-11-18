d7463b2dda6d70f75b4b510b7dc9aad873599d97
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2025-11-18 12:01:03 +0100

    Add tests for the existing `item`-based styling

---
+++ Tests/IntegrationTests/UIKit/Form/Theme/FormSeparatorItemViewThemeTests.swift
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+@_spi(AdyenInternal) @testable import Adyen
+@_spi(AdyenInternal) @testable import AdyenUI
+import XCTest
+
+final class FormSeparatorItemViewThemeTests: XCTestCase {
+
+    func test_formSeparatorItemView_shouldApplySeparatorColor() {
+        let sut = makeSUT(separatorColor: .red)
+
+        XCTAssertEqual(getSeparatorView(from: sut)?.backgroundColor, .red)
+    }
+
+    func test_formSeparatorItemView_shouldHaveCorrectHeight() {
+        let sut = makeSUT(separatorColor: .gray)
+        let expectedHeight = 1 / UIScreen.main.scale
+
+        let separatorView = getSeparatorView(from: sut)
+        let heightConstraint = separatorView?.constraints.first { $0.firstAttribute == .height }
+
+        XCTAssertEqual(heightConstraint?.constant, expectedHeight)
+    }
+
+    func test_formSeparatorItemView_convenienceInitializer_shouldUseDefaultTheme() {
+        let item = FormSeparatorItem(color: .blue)
+        let sut = FormSeparatorItemView(item: item)
+
+        XCTAssertEqual(getSeparatorView(from: sut)?.backgroundColor, .blue)
+    }
+
+    // MARK: - SUT Factory
+
+    private func makeSUT(separatorColor: UIColor) -> FormSeparatorItemView {
+        let item = FormSeparatorItem(color: separatorColor)
+        let theme = AdyenTheme.default
+        return FormSeparatorItemView(item: item, theme: theme)
+    }
+
+    // MARK: - Helpers
+
+    private func getSeparatorView(from sut: FormSeparatorItemView) -> UIView? {
+        sut.findView(by: "separatorLine")
+    }
+}
