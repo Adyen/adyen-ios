fc238859eaaeb36c38a085a927e34e142c1e1712
Naufal Aros <naufal.aros@adyen.com>
2025-11-27 19:06:09 +0100

    Update templates

---
+++ Tests/UnitTests/Generated/AutoMockable.generated.swift
@@ -0,0 +1,35 @@
+// Generated using Sourcery 2.0.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+// swiftlint:disable line_length
+// swiftlint:disable variable_name
+
+import Foundation
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
