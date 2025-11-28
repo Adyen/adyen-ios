55b666a1799fc5eb3e8af141115e7f57bf2efd68
Naufal Aros <naufal.aros@adyen.com>
2025-11-28 13:59:44 +0100

    Update AutoMockable

--- Tests/GeneratedMocks/AutoMockable.generated.swift
+++ Tests/GeneratedMocks/AutoMockable.generated.swift
@@ -1,3 +1,9 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
 // Generated using Sourcery 2.0.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 // swiftlint:disable line_length
@@ -5,33 +11,10 @@
 
 import Foundation
 #if os(iOS) || os(tvOS) || os(watchOS)
-import UIKit
+    import UIKit
 #elseif os(OSX)
-import AppKit
+    import AppKit
 #endif
-
 
 @testable import Adyen
 @testable import AdyenDropIn
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
