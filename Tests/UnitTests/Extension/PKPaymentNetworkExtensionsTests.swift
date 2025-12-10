5f5f30cdc6e37b9e635a3f9352e41aff81ad6f74
Naufal Aros <naufal.aros@adyen.com>
2025-12-10 16:46:46 +0100

    Test PKPaymentNetwork - txVariantName extension

---
+++ Tests/UnitTests/Extension/PKPaymentNetworkExtensionsTests.swift
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+@testable import AdyenComponents
+import PassKit
+import XCTest
+
+final class PKPaymentNetworkExtensionsTests: XCTestCase {
+
+    // Note: Edge case where we transform `masterCard` into `mc`
+    func test_txVariantName_givenMastercard_shouldReturnMC() throws {
+        // Given
+        let expectedVariantName = "mc"
+        let sut = PKPaymentNetwork.masterCard
+
+        // When
+        let receivedVariantName = sut.txVariantName
+
+        // Then
+        XCTAssertEqual(expectedVariantName, receivedVariantName)
+    }
+
+    // Note: Edge case where we transform `cartesBancaires` into `cartebancaire`
+    func test_txVariantName_givenCartesBancaires_shouldReturnMC() throws {
+        // Given
+        let expectedVariantName = "cartebancaire"
+        let sut = PKPaymentNetwork.cartesBancaires
+
+        // When
+        let receivedVariantName = sut.txVariantName
+
+        // Then
+        XCTAssertEqual(expectedVariantName, receivedVariantName)
+    }
+
+    func test_txVariantName_shouldReturnVariantNameLowercased() throws {
+        // Given
+        let sut = PKPaymentNetwork.JCB
+        let expectedVariantName = sut.rawValue.lowercased()
+
+        // When
+        let receivedVariantName = sut.txVariantName
+
+        // Then
+        XCTAssertEqual(expectedVariantName, receivedVariantName)
+    }
+}
