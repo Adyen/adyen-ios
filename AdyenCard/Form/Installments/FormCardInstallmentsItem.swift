ed481fcb97a0e8bf5ea3f8ebb23f8098c9db9d4e
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2026-01-22 15:35:07 +0100

    fix: remove unnecessary hiding logic as it is a default value set in init

--- AdyenCard/Form/Installments/FormCardInstallmentsItem.swift
+++ AdyenCard/Form/Installments/FormCardInstallmentsItem.swift
@@ -92,7 +92,6 @@ internal final class FormCardInstallmentsItem: BaseFormPickerItem<InstallmentEle
         // if there is no installment for the current card type
         // clear picker and hide the component
         guard !additionalPickerElements.isEmpty else {
-            isHidden.wrappedValue = true
             selectableValues = [oneTimePaymentElement.pickerElement]
             return
         }
