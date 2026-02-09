d4a4ca594081379b03bdadcfe7a19775307d6b36
robertd <robert.dalmeida@adyen.com>
2026-02-09 17:13:15 +0100

    adding the purpose of the CheckoutAttemptIdFetcher

--- AdyenCheckout/CheckoutAttemptIdFetcher.swift
+++ AdyenCheckout/CheckoutAttemptIdFetcher.swift
@@ -16,7 +16,10 @@ internal protocol CheckoutAttemptIdFetching {
 }
 
 /// Default implementation that performs the actual API call to fetch the checkout attempt ID.
-/// If there is any failure in fetching the checkoutAttemptId then we should return nil, as that would imply that there will not be any analytics send for this session.
+/// Does 2 tasks:
+/// 1. Creates a analytics environment/APIClient to send analytical events using the CheckoutConfiguration.
+/// 2. Sends a `RequestCheckoutAttemptIdRequest` to get the checkoutAttemptId.
+/// Note: If there is any failure in fetching the checkoutAttemptId then we will return nil, as that would imply that there should not be any analytics events sent.
 /// Improvement: This is an edge case and the current success rate of the api is pretty high 99 something so this would rarely ever fail. If this ever becomes a constraint we could add a retying logic to try twice if it failed once. But that is an improvement if needed alone.
 internal class CheckoutAttemptIdFetcher: CheckoutAttemptIdFetching {
 
@@ -38,15 +41,17 @@ internal class CheckoutAttemptIdFetcher: CheckoutAttemptIdFetching {
             return nil
         }
 
-        let response = try? await withCheckedThrowingContinuation { continuation in
+        guard let response = try? await withCheckedThrowingContinuation({ continuation in
             apiClient.perform(request) { result in
                 continuation.resume(with: result)
             }
+        }) else {
+            return nil
         }
         // TODO: Robert: This will need to be removed once we determine how we are going to create the AnalyticsProvider. For now we just need to inform the AnalyticProvider.
-        configuration.context.analyticsProvider?.checkoutAttemptId = response?.checkoutAttemptId
+        configuration.context.analyticsProvider?.checkoutAttemptId = response.checkoutAttemptId
 
-        return response?.checkoutAttemptId
+        return response.checkoutAttemptId
     }
 
     // MARK: - Private
