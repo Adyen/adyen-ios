cc3da7fc30a05b45b4ba697f18470c4ef421b388
Ararat Mnatsakanyan <ararat.mnatsakanyan.ararat@gmail.com>
2026-03-12 13:58:34 +0100

    chore: Update doc comment to use Nea instead of NEA

--- Adyen/Core/APIClient/Environment.swift
+++ Adyen/Core/APIClient/Environment.swift
@@ -36,7 +36,7 @@ public struct Environment: AnyAPIEnvironment {
     /// Adyen's India live  environment.
     public static let liveIndia = Environment(baseURL: URL(string: "https://checkoutshopper-live-in.adyen.com/")!)
 
-    /// Adyen's NEA live environment.
+    /// Adyen's Nea live environment.
     public static let liveNea = Environment(baseURL: URL(string: "https://checkoutshopper-live-nea.adyen.com/")!)
     
     /// Determines if the environment is one of Adyen's live environments.
