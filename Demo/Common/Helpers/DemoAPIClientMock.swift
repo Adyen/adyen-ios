7234d9933774236a1927130227117895f750684d
erenbesel <eren.besel@adyen.com>
2026-03-18 14:53:53 +0100

    Add a separate apiclient for demo app

---
+++ Demo/Common/Helpers/DemoAPIClientMock.swift
@@ -0,0 +1,31 @@
+//
+// Copyright (c) 2026 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Adyen
+import AdyenNetworking
+
+internal final class DemoAPIClientMock: APIClientProtocol {
+
+    internal var mockedResults: [Result<Response, Error>] = []
+
+    internal func perform<R: Request>(_ request: R, completionHandler: @escaping (Result<R.ResponseType, Error>) -> Void) {
+        let nextResult = mockedResults.removeFirst()
+        DispatchQueue.main.async {
+            switch nextResult {
+            case let .success(response):
+                guard let response = response as? R.ResponseType else {
+                    fatalError("""
+                    The provided Response "\(response.self)" does not match \
+                    the ResponseType of the Request "\(R.ResponseType.self)"
+                    """)
+                }
+                completionHandler(.success(response))
+            case let .failure(error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+}
