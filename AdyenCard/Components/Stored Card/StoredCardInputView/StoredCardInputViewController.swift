ccb86f1744edd7ff50ccdc367c51a918e8abff85
robertd <robert.dalmeida@adyen.com>
2026-03-19 11:02:38 +0100

    chore: Add StoredCardInputView, ViewModel, and tests

---
+++ AdyenCard/Components/Stored Card/StoredCardInputView/StoredCardInputViewController.swift
@@ -0,0 +1,251 @@
+//
+// Copyright (c) 2025 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Foundation
+import UIKit
+@_spi(AdyenInternal) import Adyen
+
+#if canImport(AdyenUI)
+    @_spi(AdyenInternal) import AdyenUI
+#endif
+
+internal class StoredCardInputViewController: UIViewController {
+
+    // MARK: - Constants
+
+    private enum Constants {
+        static let leftChevronImage = "chevron.left"
+        static let contentPadding: CGFloat = 24
+        static let distanceBetweenImageAndLabels: CGFloat = 12
+        static let distanceFromButtonsToLabels: CGFloat = 24
+        static let buttonsBottomPadding: CGFloat = 0
+        static let labelsSpacing: CGFloat = 8
+        static let buttonsSpacingWithEachOther: CGFloat = 16
+    }
+
+    // MARK: - Subviews
+
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+
+    private lazy var contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = Constants.distanceFromButtonsToLabels
+        return stackView
+    }()
+
+    private lazy var topContentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = Constants.distanceBetweenImageAndLabels
+        return stackView
+    }()
+
+    private lazy var cardImageView: CardImageView = {
+        let imageView = CardImageView(item: viewModel.cardImageItem)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.accessibilityIdentifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "cardShapedImage")
+        return imageView
+    }()
+
+    private lazy var labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = Constants.labelsSpacing
+        return stackView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.apply(theme.elements.labels.title)
+        label.numberOfLines = 0
+        label.accessibilityIdentifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "title")
+
+        return label
+    }()
+
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.accessibilityIdentifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "subTitle")
+        return label
+    }()
+
+    private lazy var securityCodeItemView: FormCardSecurityCodeItemView = {
+        // TODO: Robert: StoredView: 🐞 There is a bug(COSDK-572) with FormCardSecurityCodeItemView that when i type more than 3 characters only 3 display but validation happens with 4+ characters and then it fails validation. Needs to be debugged separately.
+        let view = FormCardSecurityCodeItemView(item: viewModel.securityCodeItem, theme: theme)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.accessibilityIdentifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "securityCodeItemView")
+        return view
+    }()
+
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = Constants.buttonsSpacingWithEachOther
+        return stackView
+    }()
+
+    private lazy var primaryButton: FormButton = {
+        let button = FormButton(buttonStyle: theme.elements.buttons.primary)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(primaryButtonTapped), for: .touchUpInside)
+        button.accessibilityIdentifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "primaryButton")
+        button.leadingImage = .adyenLock ?? .systemLock
+        return button
+    }()
+
+    private lazy var secondaryButton: FormButton = {
+        let button = FormButton(buttonStyle: theme.elements.buttons.secondary)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(secondaryButtonTapped), for: .touchUpInside)
+        button.accessibilityIdentifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "secondaryButton")
+        return button
+    }()
+
+    // MARK: - Properties
+
+    private let viewModel: StoredCardInputViewModelProtocol
+
+    private var theme: AdyenTheme {
+        viewModel.theme
+    }
+
+    // MARK: - Initializers
+
+    internal init(viewModel: StoredCardInputViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        setupViewInstructionHandler()
+    }
+
+    @available(*, unavailable)
+    internal required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View life cycle
+
+    override internal func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        viewModel.viewDidLoad()
+    }
+
+    // MARK: - setup & configurations
+
+    private func setupView() {
+        view.backgroundColor = theme.colors.background
+        // TODO: Robert: StoredView: 🐞 The scroll view isn't working in this screen. Needs separate investigation.
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentStackView)
+
+        contentStackView.addArrangedSubview(topContentStackView)
+        contentStackView.addArrangedSubview(securityCodeItemView)
+        contentStackView.addArrangedSubview(buttonsStackView)
+
+        topContentStackView.addArrangedSubview(cardImageView)
+        topContentStackView.addArrangedSubview(labelsStackView)
+
+        labelsStackView.addArrangedSubview(titleLabel)
+        labelsStackView.addArrangedSubview(subtitleLabel)
+
+        buttonsStackView.addArrangedSubview(primaryButton)
+        buttonsStackView.addArrangedSubview(secondaryButton)
+
+        configureConstraints()
+        configureContent()
+        setupNavigationBackButton()
+        configurePresentationSheet()
+    }
+
+    private func configurePresentationSheet() {
+        // Adding this to avoid swiping down to dismiss the controller.
+        isModalInPresentation = true
+    }
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+
+            contentStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: Constants.contentPadding),
+            contentStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -Constants.contentPadding),
+            contentStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -Constants.buttonsBottomPadding),
+            contentStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -Constants.contentPadding * 2)
+        ])
+    }
+
+    private func setupNavigationBackButton() {
+        let backButton = UIBarButtonItem(
+            image: UIImage(systemName: Constants.leftChevronImage),
+            style: .plain,
+            target: self,
+            action: #selector(backTapped)
+        )
+        navigationItem.leftBarButtonItem = backButton
+    }
+
+    private func configureContent() {
+        titleLabel.text = viewModel.titleText
+        subtitleLabel.attributedText = viewModel.subtitleText
+        primaryButton.title = viewModel.submitButtonTitle
+        secondaryButton.title = viewModel.showAllPaymentMethodsButtonTitle
+    }
+
+    private func updateLoadingState(_ isLoading: Bool) {
+        primaryButton.isUserInteractionEnabled = !isLoading
+        primaryButton.isEnabled = !isLoading
+        primaryButton.showsActivityIndicator = isLoading
+    }
+
+    private func setupViewInstructionHandler() {
+        viewModel.onViewInstruction = { [weak self] instruction in
+            guard let self else { return }
+            switch instruction {
+            case let .setLoading(isLoading):
+                updateLoadingState(isLoading)
+            case .showSecurityCodeValidation:
+                securityCodeItemView.resignFirstResponder()
+                securityCodeItemView.showValidation()
+            }
+        }
+    }
+
+    // MARK: - User Actions
+
+    @objc private func primaryButtonTapped() {
+        Task { @MainActor [weak self] in
+            await self?.viewModel.submit()
+        }
+    }
+
+    @objc private func secondaryButtonTapped() {
+        viewModel.showAllPaymentMethods()
+    }
+
+    @objc private func backTapped() {
+        viewModel.dismiss()
+    }
+
+}
---
+++ AdyenCard/Components/Stored Card/StoredCardInputView/StoredCardInputViewModel.swift
@@ -0,0 +1,232 @@
+//
+// Copyright (c) 2026 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+@_spi(AdyenInternal) import Adyen
+#if canImport(AdyenUI)
+    @_spi(AdyenInternal) import AdyenUI
+#endif
+#if canImport(AdyenEncryption)
+    import AdyenEncryption
+#endif
+
+internal enum StoredCardInputViewInstruction: Equatable {
+    case setLoading(Bool)
+    case showSecurityCodeValidation
+}
+
+// sourcery: AutoMockable
+internal protocol StoredCardInputViewModelProtocol: AnyObject {
+    var cardImageItem: CardImageItem { get }
+    var titleText: String { get }
+    var subtitleText: NSAttributedString { get }
+
+    var securityCodeItem: FormCardSecurityCodeItem { get }
+
+    var submitButtonTitle: String { get }
+    @MainActor func submit() async
+
+    var showAllPaymentMethodsButtonTitle: String { get }
+    @MainActor func showAllPaymentMethods()
+
+    @MainActor func dismiss()
+
+    var theme: AdyenTheme { get }
+    var onViewInstruction: Completion<StoredCardInputViewInstruction>? { get set }
+    func viewDidLoad()
+}
+
+internal final class StoredCardInputViewModel: StoredCardInputViewModelProtocol, AdyenObserver {
+
+    private enum Constants {
+        static let cardImageSizeCardArtNotAvailable = CGSize(width: 80, height: 52)
+    }
+
+    private let localizationParameters: LocalizationParameters?
+    private var storedCardPaymentMethod: StoredCardPaymentMethod
+    private let apiContext: APIContext
+    private let analyticsProvider: AnyAnalyticsProvider?
+    private let amount: Amount?
+    private let publicKey: String
+
+    internal let theme: AdyenTheme
+    internal var onViewInstruction: Completion<StoredCardInputViewInstruction>?
+
+    /// This informs the status of the payment after submitting the security code.
+    internal var cardDetailsCompletionHandler: Completion<Result<CardDetails, Error>>?
+    internal var otherPaymentOptionsHandler: VoidCompletion?
+    internal var closeHandler: VoidCompletion?
+
+    internal init(
+        theme: AdyenTheme,
+        storedCardPaymentMethod: StoredCardPaymentMethod,
+        apiContext: APIContext,
+        publicKey: String,
+        amount: Amount?,
+        analyticsProvider: AnyAnalyticsProvider?,
+        localizationParameters: LocalizationParameters?
+    ) {
+        self.theme = theme
+        self.storedCardPaymentMethod = storedCardPaymentMethod
+        self.amount = amount
+        self.apiContext = apiContext
+        self.publicKey = publicKey
+        self.localizationParameters = localizationParameters
+        self.analyticsProvider = analyticsProvider
+    }
+
+    internal lazy var cardImageItem: AdyenUI.CardImageItem = {
+        let displayInformation = storedCardPaymentMethod.displayInformation(using: localizationParameters)
+        let imageURL = LogoURLProvider.logoURL(
+            withName: displayInformation.logoName,
+            environment: apiContext.environment,
+            size: .large
+        )
+        return CardImageItem(
+            imageURL: imageURL,
+            sizeMode: .fixed(Constants.cardImageSizeCardArtNotAvailable),
+            theme: theme
+        )
+    }()
+
+    internal lazy var securityCodeItem: FormCardSecurityCodeItem = {
+        let item = FormCardSecurityCodeItem()
+        item.identifier = ViewIdentifierBuilder.build(scopeInstance: self, postfix: "securityCodeItem")
+        return item
+    }()
+
+    internal var titleText: String {
+        localizedString(.cardComponentInputTitle, localizationParameters)
+    }
+
+    /// We construct something like - Enter the security code for BOLD[Visa •••• 4556] to complete the payment of BOLD[$140.98]
+    internal var subtitleText: NSAttributedString {
+        let displayInformation = storedCardPaymentMethod.displayInformation(using: localizationParameters)
+        let paymentMethodTitle = storedCardPaymentMethod.name + displayInformation.title
+        let localizedString = localizedString(.cardComponentInputDescription, localizationParameters, paymentMethodTitle, formattedAmount)
+
+        let attributed = NSMutableAttributedString(string: localizedString)
+
+        let range = (localizedString as NSString).range(of: paymentMethodTitle)
+        attributed.addAttribute(.font, value: theme.elements.labels.bodyEmphasized.font, range: range)
+        attributed.addAttribute(.foregroundColor, value: theme.elements.labels.bodyEmphasized.color, range: range)
+
+        let amountRange = (localizedString as NSString).range(of: formattedAmount)
+        attributed.addAttribute(.font, value: theme.elements.labels.bodyEmphasized.font, range: amountRange)
+        attributed.addAttribute(.foregroundColor, value: theme.elements.labels.bodyEmphasized.color, range: amountRange)
+
+        return attributed
+    }
+
+    private var formattedAmount: String {
+        guard let amount,
+              let formatted = AmountFormatter.formatted(
+                  amount: amount.value,
+                  currencyCode: amount.currencyCode
+              ) else {
+            return ""
+        }
+        return formatted
+    }
+
+    internal var submitButtonTitle: String {
+        localizedString(.submitButtonFormatted, localizationParameters, formattedAmount)
+    }
+
+    internal func viewDidLoad() {
+        sendDidLoadEvent()
+    }
+
+    @MainActor internal func dismiss() {
+        resetSecurityCodeField()
+        closeHandler?()
+    }
+
+    // MARK: - Other payment options
+
+    internal var showAllPaymentMethodsButtonTitle: String {
+        localizedString(.preselectedPaymentMethodOtherOptions, localizationParameters)
+    }
+
+    @MainActor internal func showAllPaymentMethods() {
+        resetSecurityCodeField()
+        otherPaymentOptionsHandler?()
+    }
+
+    // MARK: - Submit payment
+
+    @MainActor internal func submit() async {
+        guard securityCodeItem.isValid() else {
+            onViewInstruction?(.showSecurityCodeValidation)
+            return
+        }
+
+        onViewInstruction?(.setLoading(true))
+
+        let securityCode: String = securityCodeItem.value
+        resetSecurityCodeField()
+
+        await submitPayment(securityCode: securityCode)
+        onViewInstruction?(.setLoading(false))
+    }
+
+    internal func submitPayment(securityCode: String) async {
+        do {
+            let encryptedCardDetails: CardDetails = try {
+                do {
+                    return try encryptCardDetails(
+                        securityCode: securityCode,
+                        cardPublicKey: publicKey
+                    )
+                } catch {
+                    sendEncryptionErrorEvent()
+                    throw error
+                }
+            }()
+            cardDetailsCompletionHandler?(.success(encryptedCardDetails))
+        } catch {
+            cardDetailsCompletionHandler?(.failure(error))
+        }
+    }
+
+    @MainActor internal func resetSecurityCodeField() {
+        securityCodeItem.value = ""
+    }
+
+    private func encryptCardDetails(
+        securityCode: String,
+        cardPublicKey: String
+    ) throws -> CardDetails {
+        let encryptedSecurityCode = try CardEncryptor.encrypt(
+            securityCode: securityCode,
+            with: cardPublicKey
+        )
+        return CardDetails(
+            paymentMethod: storedCardPaymentMethod,
+            encryptedSecurityCode: encryptedSecurityCode
+        )
+    }
+
+    // MARK: - Events
+
+    private func sendEncryptionErrorEvent() {
+        var errorEvent = AnalyticsEventError(
+            component: storedCardPaymentMethod.type.rawValue,
+            type: .internal
+        )
+        errorEvent.code = AnalyticsConstants.ErrorCode.encryptionError.stringValue
+        analyticsProvider?.add(error: errorEvent)
+    }
+
+    private func sendDidLoadEvent() {
+        var infoEvent = AnalyticsEventInfo(
+            component: storedCardPaymentMethod.type.rawValue,
+            type: .rendered
+        )
+        infoEvent.isStoredPaymentMethod = true
+        infoEvent.brand = storedCardPaymentMethod.brand.rawValue
+        analyticsProvider?.add(info: infoEvent)
+    }
+}
