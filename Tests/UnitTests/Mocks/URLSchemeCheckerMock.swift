185c9c729ce8ebeeebaf6871ca421f2bf8c0540b
Naufal Aros <naufal.aros@adyen.com>
2026-04-02 16:43:53 +0200

    Add URLSchemeCheckerMock

---
+++ Tests/UnitTests/Mocks/URLSchemeCheckerMock.swift
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2026 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+@_spi(AdyenInternal) @testable import Adyen
+import Foundation
+
+class URLSchemeCheckerMock: URLSchemeChecking {
+    var openableSchemes: Set<String> = []
+
+    func canOpen(scheme: String) -> Bool {
+        openableSchemes.contains(scheme)
+    }
+}
