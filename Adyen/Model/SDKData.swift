755c1e2c294a72dab2162c5a61cdcf9f1c9ec83b
Naufal Aros <naufal.aros@adyen.com>
2026-05-11 12:37:51 +0200

    Update SDKData with new props

--- Adyen/Model/SDKData.swift
+++ Adyen/Model/SDKData.swift
@@ -7,45 +7,65 @@
 import Foundation
 
 package struct SDKData: Codable {
-    
+
     internal struct Analytics: Codable {
         internal let checkoutAttemptId: String
     }
-    
+
     package struct Authentication: Codable {
         internal let threeDS2SdkVersion: String
-        
+
         package init(threeDS2SdkVersion: String) {
             self.threeDS2SdkVersion = threeDS2SdkVersion
         }
     }
-    
+
+    package enum PaymentMethodBehavior: String, Codable {
+        /// Indicates that the SDK has a specific component for this payment method.
+        case nativeComponent
+
+        /// Indicates that the SDK does not have native component support for this payment method
+        /// and will handle it through the Instant Payment Component.
+        case genericComponent
+    }
+
     internal let analytics: Analytics
     internal private(set) var authentication: Authentication?
     internal let schemaVersion: Int = SchemaVersions.v1
     private let supportNativeRedirect: Bool = true
     private let timestamp = Int(Date().timeIntervalSince1970 * 1000)
-    
+
+    private let paymentMethodBehavior: PaymentMethodBehavior
+    private let channel: String = checkoutPlatformParams.channel
+    private let platform: String = checkoutPlatformParams.platform.rawValue
+    private let sdkVersion: String = checkoutPlatformParams.version
+
     package var encodedValue: String? {
         try? AdyenCoder.encodeBase64(self)
     }
-    
+
     internal init(
         checkoutAttemptId: String,
+        paymentMethodBehavior: PaymentMethodBehavior,
         authenticationProvider: SDKDataAuthenticationProvider? = nil
     ) {
         self.analytics = .init(checkoutAttemptId: checkoutAttemptId)
+        self.paymentMethodBehavior = paymentMethodBehavior
         self.authentication = authenticationProvider?.authentication
     }
-    
+
     private enum CodingKeys: String, CodingKey {
         case analytics
         case authentication
         case supportNativeRedirect
         case schemaVersion
         case timestamp = "createdAt"
+        case paymentMethodBehavior
+        case channel
+        case platform
+        case sdkVersion
     }
-    
+
     private enum SchemaVersions {
         internal static let v1 = 1
     }
@@ -53,4 +73,18 @@ package struct SDKData: Codable {
 
 package protocol SDKDataAuthenticationProvider {
     var authentication: SDKData.Authentication { get }
+}
+
+package extension PaymentComponent {
+
+    var paymentMethodBehavior: SDKData.PaymentMethodBehavior {
+        .nativeComponent
+    }
+}
+
+package extension InstantPaymentComponent {
+
+    var paymentMethodBehavior: SDKData.PaymentMethodBehavior {
+        .genericComponent
+    }
 }
