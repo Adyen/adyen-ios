44ca345af7af9541608204ef2f488035f97a0d67
robertd <robert.dalmeida@adyen.com>
2026-05-19 12:11:22 +0200

    Remove @_spi(AdyenInternal) import for AnyNavigationBar and replace with package access control

--- AdyenUI/UI/ToolBar/ModalToolbar.swift
+++ AdyenUI/UI/ToolBar/ModalToolbar.swift
@@ -4,7 +4,7 @@
 // This file is open source and available under the MIT license. See the LICENSE file for more info.
 //
 
-@_spi(AdyenInternal) import protocol Adyen.AnyNavigationBar
+import Adyen
 import UIKit
 
 package class ModalToolbar: UIView, AnyNavigationBar {
