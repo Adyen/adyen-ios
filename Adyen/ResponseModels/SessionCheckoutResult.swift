94524c1d3bf73c41f40728e6436a11fa07efdfc3
erenbesel <eren.besel@adyen.com>
2026-06-12 15:18:03 +0200

    feature: Add SessionCheckoutResult and AdvancedCheckoutResult types

---
+++ Adyen/ResponseModels/SessionCheckoutResult.swift
@@ -0,0 +1,26 @@
+//
+// Copyright (c) 2026 Adyen N.V.
+//
+// This file is open source and available under the MIT license. See the LICENSE file for more info.
+//
+
+import Foundation
+
+/// The result of a completed session-based checkout flow.
+public struct SessionCheckoutResult {
+
+    /// The result code indicating the outcome of the payment.
+    public let resultCode: CheckoutResultCode
+
+    /// The session identifier.
+    public let sessionId: String
+
+    /// An encoded string that can be used to get the payment outcome on your server.
+    public let sessionResult: String
+
+    package init(resultCode: CheckoutResultCode, sessionId: String, sessionResult: String) {
+        self.resultCode = resultCode
+        self.sessionId = sessionId
+        self.sessionResult = sessionResult
+    }
+}
