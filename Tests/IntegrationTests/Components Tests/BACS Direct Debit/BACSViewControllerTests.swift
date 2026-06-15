99b8881f63a1bb121d1fee2ef529627f1dbd4b53
Naufal Aros <naufal.aros@adyen.com>
2026-06-15 16:42:28 +0200

    Fix compiler errors in BACSViewControllerTests

--- Tests/IntegrationTests/Components Tests/BACS Direct Debit/BACSViewControllerTests.swift
+++ Tests/IntegrationTests/Components Tests/BACS Direct Debit/BACSViewControllerTests.swift
@@ -12,46 +12,84 @@ import XCTest
 class BACSViewControllerTests: XCTestCase {
 
     var sut: BACSViewController!
-    var presenter: BACSInputPresenterProtocolMock!
+    var tracker: BACSDirectDebitComponentTrackerProtocolMock!
+    var itemsFactory: BACSItemsFactoryProtocolMock!
+    var viewModel: BACSViewModel!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        presenter = BACSInputPresenterProtocolMock()
 
-        let styleProvider = FormComponentStyle()
+        tracker = BACSDirectDebitComponentTrackerProtocolMock()
+        itemsFactory = makeItemsFactoryMock()
+
+        let paymentMethod = BACSDirectDebitPaymentMethod(type: .bacsDirectDebit, name: "BACS Direct Debit")
+        let amount = Amount(value: 105.7, currencyCode: "USD", localeIdentifier: nil)
+        let configuration = BACSDirectDebitComponent.Configuration(showsSubmitButton: true)
+
+        viewModel = BACSViewModel(
+            paymentMethod: paymentMethod,
+            amount: amount,
+            configuration: configuration,
+            tracker: tracker,
+            itemsFactory: itemsFactory,
+            onSubmit: { _ in }
+        )
+
         sut = BACSViewController(
             title: "BACS Direct Debit",
-            scrollEnabled: true,
-            styleProvider: styleProvider
+            viewModel: viewModel
         )
-        sut.viewModel = presenter
     }
 
     override func tearDownWithError() throws {
-        presenter = nil
+        tracker = nil
+        itemsFactory = nil
+        viewModel = nil
         sut = nil
         try super.tearDownWithError()
     }
 
-    func testTitleIsSetOnCreation() throws {
+    func test_title_shouldBeSetOnCreation() throws {
         // When
         let title = try XCTUnwrap(sut.title)
         XCTAssertFalse(title.isEmpty)
     }
 
-    func testViewDidLoadShouldCallPresenterViewDidLoad() {
+    func test_viewDidLoad_shouldCallViewModelViewDidLoad() {
         // When
         sut.viewDidLoad()
 
         // Then
-        XCTAssertTrue(presenter.viewDidLoadCalled)
+        XCTAssertEqual(tracker.initialEventCallsCount, 1)
+        XCTAssertEqual(tracker.didLoadEventCallsCount, 1)
     }
 
-    func testViewWillAppearShouldCallPresenterViewWillAppear() {
-        // When
-        sut.viewWillAppear(true)
+    // MARK: - Private
 
-        // Then
-        XCTAssertTrue(presenter.viewWillAppearCalled)
+    private func makeItemsFactoryMock() -> BACSItemsFactoryProtocolMock {
+        let styleProvider = FormComponentStyle()
+        let itemsFactory = BACSItemsFactoryProtocolMock()
+
+        let holderNameItem = FormTextInputItem()
+        holderNameItem.validator = LengthValidator(minimumLength: 1, maximumLength: 70)
+        itemsFactory.createHolderNameItemReturnValue = holderNameItem
+
+        let bankAccountNumberItem = FormTextInputItem()
+        bankAccountNumberItem.validator = NumericStringValidator(minimumLength: 1, maximumLength: 8)
+        itemsFactory.createBankAccountNumberItemReturnValue = bankAccountNumberItem
+
+        let sortCodeItem = FormTextInputItem()
+        sortCodeItem.validator = NumericStringValidator(minimumLength: 1, maximumLength: 6)
+        itemsFactory.createSortCodeItemReturnValue = sortCodeItem
+
+        let emailItem = FormTextInputItem()
+        emailItem.validator = EmailValidator()
+        itemsFactory.createEmailItemReturnValue = emailItem
+
+        itemsFactory.createPaymentButtonReturnValue = FormButtonItem(style: styleProvider.mainButtonItem)
+        itemsFactory.createAmountConsentToggleAmountReturnValue = FormToggleItem()
+        itemsFactory.createLegalConsentToggleReturnValue = FormToggleItem()
+
+        return itemsFactory
     }
 }
