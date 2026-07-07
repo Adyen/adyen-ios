b69e559e28d886da384283d7278615fc524d7296
robertd <robert.dalmeida@adyen.com>
2026-07-07 13:22:02 +0200

    test: Remove ineffective empty-apartment billing address test
    test_billingAddress_withEmptyApartment_shouldSubmitNilApartment was misnamed and weak:
    despite shouldSubmitNilApartment it only asserted validAddress != nil, never apartment == nil.
    
    The nil-apartment behavior is covered by the full PostalAddress equality assertion in
    full_withPrefilledBillingAddress_prefillsAndSubmitsAddress (its address has apartment nil),
    and the present-apartment case by full_withApartmentValue_includesApartmentInSubmittedAddress
    in CardComponentBillingAddressModeTests.

--- Tests/IntegrationTests/Card Tests/CardComponentTests.swift
+++ Tests/IntegrationTests/Card Tests/CardComponentTests.swift
@@ -1696,60 +1696,6 @@ class CardComponentTests: XCTestCase {
         XCTAssertTrue(items.holderNameItem.isValid())
     }
 
-    func test_billingAddress_withEmptyApartment_shouldSubmitNilApartment() throws {
-        var configuration = CardConfiguration()
-        configuration.billingAddressMode = .full(supportedCountryCodes: ["US"], hideForCardBrands: [])
-
-        let cardBrandProviderMock = BinInfoProviderMock()
-        cardBrandProviderMock.onFetch = {
-            $0(BinLookupResponse(
-                brands: [DetectedCardBrand(brand: .visa)],
-                issuingCountryCode: "US"
-            ))
-        }
-
-        let sut = CardComponent(
-            paymentMethod: method,
-            context: context,
-            configuration: configuration,
-            binProvider: cardBrandProviderMock
-        )
-
-        let delegate = PaymentComponentDelegateMock()
-        sut.delegate = delegate
-
-        setupRootViewController(sut.viewController)
-
-        let view: UIView = sut.cardViewController.view
-
-        let securityCodeField: FormCardSecurityCodeItemView = try XCTUnwrap(view.findView(by: CardViewIdentifier.securityCode))
-        let expiryDateField: FormTextInputItemView = try XCTUnwrap(view.findView(by: CardViewIdentifier.expiryDate))
-        let numberField: FormCardNumberItemView = try XCTUnwrap(view.findView(by: CardViewIdentifier.cardNumber))
- 
-        let billingAddressView: FormAddressPickerItemView = try XCTUnwrap(view.findView(by: CardViewIdentifier.billingAddress))
-
-        populate(textItemView: securityCodeField, with: "737")
-        populate(textItemView: numberField, with: "4111 1120 1426 7661")
-        populate(textItemView: expiryDateField, with: "12/30")
-
-        billingAddressView.item.value = PostalAddress(city: "Seattle", postalCode: "123", stateOrProvince: "AZ", street: "Test Street")
-
-        let delegateExpectation = expectation(description: "PaymentComponentDelegate must be called when submit button is clicked.")
-        delegate.onDidFail = { error, component in XCTFail("should not fail") }
-        delegate.onDidSubmit = { data, component in
-            XCTAssertTrue(component === sut)
-            XCTAssertTrue(data.paymentMethod is CardDetails)
-            XCTAssertNotNil(sut.cardViewController.validAddress)
-
-            sut.stopLoading()
-            delegateExpectation.fulfill()
-        }
-
-        tapSubmitButton(on: sut.viewController.view)
-
-        waitForExpectations(timeout: 10, handler: nil)
-    }
-
     func test_expiryDateField_whenValueEntered_shouldParseMonthAndYear() {
         let sut = CardComponent(
             paymentMethod: method,
