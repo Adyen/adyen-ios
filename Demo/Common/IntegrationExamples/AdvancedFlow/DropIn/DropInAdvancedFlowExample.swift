e47506de1fa86e25bb5760483d40361231d5a17f
erenbesel <eren.besel@adyen.com>
2026-08-26 11:27:13 +0200

    Remove unnecessary let binding

--- Demo/Common/IntegrationExamples/AdvancedFlow/DropIn/DropInAdvancedFlowExample.swift
+++ Demo/Common/IntegrationExamples/AdvancedFlow/DropIn/DropInAdvancedFlowExample.swift
@@ -304,7 +304,7 @@ extension DropInAdvancedFlowExample: StoredPaymentMethodsDelegate {
             switch result {
             case .success:
                 completion(true)
-            case let .failure:
+            case .failure:
                 completion(false)
             }
         }
