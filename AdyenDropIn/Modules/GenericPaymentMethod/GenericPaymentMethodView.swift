6ed54457d1725983e1bcc23712c24645adbcd4e9
Naufal Aros <naufal.aros@adyen.com>
2026-09-10 17:15:10 +0200

    Initial generic PM view

--- AdyenDropIn/Modules/GenericPaymentMethod/GenericPaymentMethodView.swift
+++ AdyenDropIn/Modules/GenericPaymentMethod/GenericPaymentMethodView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 internal struct GenericPaymentMethodView: View {
 
+    private enum Constants {
+        static let logoFrame = CGSize(width: 80, height: 52)
+    }
+
     // MARK: - Properties
 
     @ObservedObject internal var viewModel: GenericPaymentMethodViewModel
@@ -18,16 +22,38 @@ internal struct GenericPaymentMethodView: View {
 
     internal var body: some View {
         VStack {
-            AsyncImage(url: viewModel.paymentMethodLogoURL)
-            Text("Logo")
-            Text("\(viewModel.paymentMethodName)")
-            Text("Description")
-            Text("Loading")
+            AsyncImage(url: viewModel.paymentMethodLogoURL) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
 
-            ProgressView()
-                .task {
-                    viewModel.startPayment()
-                }
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: Constants.logoFrame.width, height: Constants.logoFrame.height)
+
+            VStack(spacing: 16) {
+                Text("\(viewModel.paymentMethodName)")
+                    .font(Font(theme.elements.labels.title.font))
+                Text("You will be guided to the next step of the process.")
+                    .font(Font(theme.elements.labels.body.font))
+            }
+            .foregroundStyle(Color(uiColor: theme.colors.text))
+            .multilineTextAlignment(.center)
+            .padding(.top, 32)
+
+            ProgressView {
+                Text("Processing")
+            }
+            .task {
+                viewModel.startPayment()
+
+            }
+            .padding(.top, 64)
         }
+        .padding()
+        .background(Color(uiColor: theme.colors.background))
     }
+
+    // MARK: - Private
 }
