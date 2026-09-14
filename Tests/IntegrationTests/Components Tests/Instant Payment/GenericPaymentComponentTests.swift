993f63f4c82c861be40b96d1de39b6b61da09137
Naufal Aros <naufal.aros@adyen.com>
2026-09-14 18:09:45 +0200

    Add tests for GenericPaymentComponent's PaymentButtonViewController

--- Tests/IntegrationTests/Components Tests/Instant Payment/GenericPaymentComponentTests.swift
+++ Tests/IntegrationTests/Components Tests/Instant Payment/GenericPaymentComponentTests.swift
@@ -59,6 +59,25 @@ class GenericPaymentComponentTests: XCTestCase {
         waitForExpectations(timeout: 2, handler: nil)
     }
 
+    func testViewControllerHasCorrectTitle() {
+        XCTAssertEqual(sut.viewController.title, paymentMethod.displayInformation(using: nil).title)
+    }
+
+    func testTappingPayButtonShouldCallPaymentComponentDelegateDidSubmit() {
+        sut.viewController.loadViewIfNeeded()
+
+        let didSubmitExpectation = expectation(description: "PaymentComponentDelegate must be called.")
+        delegate.onDidSubmit = { data, component in
+            XCTAssertTrue(component === self.sut)
+            didSubmitExpectation.fulfill()
+        }
+
+        let payButtonItemViewButton: UIControl? = sut.viewController.view.findView(by: "payButtonItem.button")
+        payButtonItemViewButton?.sendActions(for: .touchUpInside)
+
+        wait(for: [didSubmitExpectation], timeout: 10)
+    }
+
     // MARK: - Private
 
     private var paymentComponentData: PaymentComponentData {
